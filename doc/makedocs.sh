5d3d0b74695de4a9b5599d1f2d6268f87775028e
uzadow <uzadow@libavg.de>
2004-04-20 21:21:12 +0000

    Fixed hyphenation problem in html manpages.
    svn path=/trunk/libavg/; revision=1193

--- doc/makedocs.sh
+++ doc/makedocs.sh
@@ -2,6 +2,6 @@
 cd reference
 doxygen Doxyfile
 cd -
-man ../man/avg.1 | man2html > man_avg.html
-man ../man/avgrc.5 | man2html > man_avgrc.html
+man ../man/avg.1 | man2html -title avg avg > man_avg.html
+man ../man/avgrc.5 | man2html -title avgrc avgrc > man_avgrc.html
 
