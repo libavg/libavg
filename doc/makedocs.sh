dae2c36de2b7c5224bafa5f183c0141572562664
uzadow <uzadow@libavg.de>
2005-10-03 20:43:03 +0000

    New reference documentation.
    svn path=/trunk/libavg/; revision=1318

--- doc/makedocs.sh
+++ doc/makedocs.sh
@@ -1,7 +1,6 @@
 #/bin/bash
-cd reference
-doxygen Doxyfile
-cd -
-man ../man/avg.1 | man2html -title avg avg > man_avg.html
-man ../man/avgrc.5 | man2html -title avgrc avgrc > man_avgrc.html
+epydoc --html -o reference -n libavg -u ../index.html --no-private --docformat plaintext --no-frames  /usr/local/lib/python2.4/site-packages/libavg/avg.so
+cp ../avg.dtd ./avg_dtd.txt
+#man ../man/avg.1 | man2html -title avg avg > man_avg.html
+#man ../man/avgrc.5 | man2html -title avgrc avgrc > man_avgrc.html
 
