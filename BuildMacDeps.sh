7a6b0e74f8425738b81b5330a58536e8771a7f75
uzadow <uzadow@libavg.de>
2006-07-25 19:57:46 +0000

    Improved unfinished BuildMacDeps.sh
    svn path=/trunk/libavg/; revision=1476

--- BuildMacDeps.sh
+++ BuildMacDeps.sh
@@ -31,6 +31,46 @@ buildlibjpeg()
     cd ..
 }
 
+buildglib()
+{
+    cd glib-2.10.0 
+    LDFLAGS="-framework CoreFoundation $LDFLAGS" ./configure  --prefix=${AVG_PATH} --disable-shared
+    make clean
+    LDFLAGS="-framework CoreFoundation $LDFLAGS" make
+    make install
+    cd ..
+}
+
+buildpango()
+{
+    cd pango-1.13.3
+    LDFLAGS="-framework CoreFoundation $LDFLAGS" ./configure  --prefix=${AVG_PATH} --disable-shared
+    make clean
+    LDFLAGS="-framework CoreFoundation $LDFLAGS" make
+    make install
+    cd ..
+}
+
+fixpkgconfig()
+{
+  Filename=$1
+  sed 's/PREFIX/prefix/' ${Filename} > ${Filename}.tmp
+  mv ${Filename}.tmp ${Filename}
+}
+
+buildffmpeg()
+{
+    cd ffmpeg
+    ./configure --prefix=${AVG_PATH} --disable-shared
+    make clean
+    make
+    fixpkgconfig libavcodec.pc 
+    fixpkgconfig libavformat.pc 
+    fixpkgconfig libavutil.pc 
+    make install
+    cd ..    
+}
+
 if [[ x"${AVG_PATH}" == x"" ]]
 then
     echo Please set AVG_PATH
@@ -48,10 +88,15 @@ buildLib tiff-3.8.2 --disable-shared
 buildLib zlib-1.2.3
 buildLib libpng-1.2.12 --disable-shared 
 buildLib ImageMagick-6.2.8 "--disable-shared --without-x --without-fontconfig --without-freetype --without-perl --without-xml"
-buildLib ffmpeg --disable-shared
+buildLib pkg-config-0.20
+buildffmpeg
 buildLib SDL-1.2.11 "--disable-shared --disable-audio --disable-cdrom --disable-threads --disable-file --disable-video-x11 --without-x"
-buildiLib pkg-config-0.20
-buildiLib gettext-0.14.5 --disable-shared 
-buildiLib glib-2.10.0 --disable-shared
+buildLib gettext-0.14.6 "--disable-shared --with-included-gettext"
+buildglib
+buildLib freetype-2.1.10 --disable-shared
+buildLib expat-2.0.0 --disable-shared 
+buildLib fontconfig-2.3.1 "--disable-shared --with-add-fonts=/Library/Fonts,/Network/Library/Fonts,/System/Library/Fonts"
+buildpango
+buildLib boost_1_33_1 --with-libraries=python 
 
 cd ../libavg
