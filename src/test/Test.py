5b9f129e1ccc757fa8e7c4ef1ebf0ff6a979052c
uzadow <uzadow@libavg.de>
2006-08-12 20:48:01 +0000

    More make check fixes.
    svn path=/trunk/libavg/; revision=1528

--- src/test/Test.py
+++ src/test/Test.py
@@ -3,8 +3,7 @@
 import unittest
 
 import sys, syslog, time, os
-sys.path.append('../python/.libs')
-sys.path.append('../python')
+sys.path = ['../python/.libs', '../python']
 import avg
 import anim
 
