3b514c9391c28dd2960db096ff88a82101a43d88
uzadow <uzadow@libavg.de>
2006-09-23 14:44:09 +0000

    Mac build cleanup.
    svn path=/trunk/libavg/; revision=1609

--- BuildMacDeps.sh
+++ BuildMacDeps.sh
@@ -3,13 +3,24 @@
 set -e
 set -x
 
+clean()
+{
+    rm -rf ${AVG_PATH}/bin/
+    rm -rf ${AVG_PATH}/lib/
+    rm -rf ${AVG_PATH}/include/
+
+    mkdir ${AVG_PATH}/bin
+    mkdir ${AVG_PATH}/lib
+    mkdir ${AVG_PATH}/include
+}
+
 buildLib()
 {
     LIBNAME=$1
     CONFIG_ARGS=$2
 
     cd ${LIBNAME}
-    ./configure --prefix=${AVG_PATH} $2
+    ./configure --prefix=${AVG_PATH} ${CONFIG_ARGS}
     make clean
     make -j3
     make install
@@ -60,10 +71,12 @@ buildpango()
     cd ..
 }
 
-buildffmpeg()
+buildfontconfig()
 {
-    cd ffmpeg
-    ./configure --prefix=${AVG_PATH} --disable-shared --disable-debug --disable-encoders
+    cd fontconfig-2.3.2
+#    patch fontconfig.pc.in ../../libavg/macpatches/fontconfig.pc.in.diff
+#    patch -p1 <../../libavg/macpatches/fontconfig-2.3.2-noftinternals.patch
+     LDFLAGS="-framework ApplicationServices ${LDFLAGS}" ./configure --prefix=${AVG_PATH} --disable-shared --with-add-fonts=/Library/Fonts,/System/Library/Fonts,~/fonts --with-confdir=/etc/fonts
     make clean
     make -j3
     make install
@@ -76,13 +89,7 @@ then
     exit -1 
 fi
 
-rm -rf ${AVG_PATH}/bin/
-rm -rf ${AVG_PATH}/lib/
-rm -rf ${AVG_PATH}/include/
-
-mkdir ${AVG_PATH}/bin
-mkdir ${AVG_PATH}/lib
-mkdir ${AVG_PATH}/include
+clean()
 
 export CFLAGS=-O2
 
@@ -96,15 +103,17 @@ buildLib zlib-1.2.3
 buildlibpng
 buildLib ImageMagick-6.2.8 "--without-x --without-fontconfig --without-freetype --without-perl --disable-delegate-build --without-modules --without-bzlib"
 buildLib pkg-config-0.20
-buildffmpeg
+buildLib ffmpeg "--disable-shared --disable-debug"
 buildLib SDL-1.2.11 "--disable-shared --disable-audio --disable-cdrom --disable-threads --disable-file --disable-video-x11 --without-x"
 buildLib gettext-0.14.6 "--disable-shared --with-included-gettext --disable-csharp  --disable-libasprintf"
 buildglib
-buildLib freetype-2.1.10 "--disable-shared" # --with-old-mac-fonts"
+
+buildLib freetype-2.1.10 "--disable-shared"
+#buildLib freetype-2.2.1 "--disable-shared --with-old-mac-fonts"
 buildLib expat-2.0.0 --disable-shared
 
-#patch fontconfig-2.3.1/fontconfig.pc.in ../libavg/macpatches/fontconfig.pc.in.diff
-buildLib fontconfig-2.3.1 "--disable-shared --with-add-fonts=/usr/share/fonts,/Library/Fonts,/System/Library/Fonts,~/fonts --with-confdir=/etc/fonts"
+buildfontconfig
+#buildLib fontconfig-2.3.2 "--disable-shared --with-add-fonts=/Library/Fonts,/System/Library/Fonts,~/fonts --with-confdir=/etc/fonts"
 
 buildpango
 buildLib boost_1_33_1 --with-libraries=python 
