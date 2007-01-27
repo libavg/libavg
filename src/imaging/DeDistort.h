a851acc12609fbf2bfb161385823f15f08b14d37
igor <igor@libavg.de>
2007-01-27 19:59:13 +0000

    Fixed constructor...
    svn path=/trunk/libavg/; revision=1902

--- src/imaging/DeDistort.h
+++ src/imaging/DeDistort.h
@@ -12,7 +12,14 @@ namespace avg {
 class DeDistort: public CoordTransformer
 {
     public:
-        DeDistort(IntRect srcRect, double K1, double T, double RescaleFactor = 1);
+        DeDistort::DeDistort(DPoint &FilmDisplacement, 
+                DPoint &FilmScale, 
+                std::vector<double> DistortionParams, 
+                double P[3], 
+                double N[3], 
+                double Angle, 
+                DPoint m_DisplayScale, 
+                DPoint m_DisplayDisplacement );
         virtual ~DeDistort();
         virtual DPoint transform_point(const DPoint & pt); //(x,y) -> (x', y')
         virtual DPoint inverse_transform_point(const DPoint & pt); //(x,y) -> (x', y')
