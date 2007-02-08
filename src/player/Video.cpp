4a387edd271d7f0f4c53f12dcf4ac94008423b43
uzadow <uzadow@libavg.de>
2007-02-08 10:56:41 +0000

    Fixed mesa video decoding.
    svn path=/trunk/libavg/; revision=1936

--- src/player/Video.cpp
+++ src/player/Video.cpp
@@ -193,7 +193,7 @@ bool Video::renderToSurface(ISurface * pSurface)
     } else {
         BitmapPtr pBmp = pSurface->lockBmp();
         m_bEOF = m_pDecoder->renderToBmp(pBmp);
-        if (ycbcrMode== DisplayEngine::OGL_MESA) {
+        if (ycbcrMode == DisplayEngine::OGL_MESA && pBmp->getPixelFormat() == YCbCr422) {
             FilterFlipUV().applyInPlace(pBmp);
         }   
     }
