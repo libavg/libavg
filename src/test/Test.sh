8a3d1d6e3f409384e3fce8c124097d5a6899559e
uzadow <uzadow@libavg.de>
2007-08-18 20:55:07 +0000

    Fixed Test.sh.
    svn path=/trunk/libavg/; revision=2238

--- src/test/Test.sh
+++ src/test/Test.sh
@@ -4,7 +4,7 @@ set -e
 runTest()
 {
     echo
-    echo "---------- OGL, BPP=$1 POW2=$2, YCbCr=$3 RGB=$4 PBO=$5 -----------"
+    echo "---------- OGL, BPP=$1 POW2=$2, YCbCr=$3 PBO=$5 -----------"
     echo
     ./Test.py OGL $1 $2 $3 $4 $5
 }
@@ -12,23 +12,23 @@ runTest()
 testOGL()
 {
 # Test real-life OpenGL configurations:
-#       BPP POW2  YCbCr  RGB   PBO
+#       BPP POW2  YCbCr  PBO
 # Linux NVidia GeForce 6x00 and Mac OS X GeForce FX
-runTest $1  false shader false true
+runTest $1  false shader true
 if [[ `uname` == Linux ]]
 then
     # Older Linux NVidia
-    runTest $1  false none   false true
+    runTest $1  false none   true
     # MESA Matrox (not sure about pow2...)
-    runTest $1  false mesa   false false
+    runTest $1  false mesa   false
 else
     # Mac OS X 10.2.6, NVidia GeForce2 MX
-    runTest $1  false apple  false false
+    runTest $1  false apple  false
     # Mac OS X 10.2.6, ATi Rage 128 pro 
-    runTest $1  true  apple  false false
+    runTest $1  true  apple  false
 fi
 # A card that doesn't support anything at all.
-runTest $1  true none    false false
+runTest $1  true none    false
 }
 
 cd ${0%/*}
