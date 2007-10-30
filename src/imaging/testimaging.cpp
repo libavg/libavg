e93581a3c37565b3ea36301b3b53dc76e147bd2d
uzadow <uzadow@libavg.de>
2007-10-30 14:25:27 +0000

    testimaging now works under windows.
    svn path=/trunk/libavg/; revision=2377

--- src/imaging/testimaging.cpp
+++ src/imaging/testimaging.cpp
@@ -34,9 +34,11 @@
 #include <boost/bind.hpp>
 
 #include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #include <sstream>
 
