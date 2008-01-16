897b2aefeea817ace097e1795e3c54735aaf2d8d
uzadow <uzadow@libavg.de>
2008-01-16 10:57:30 +0000

    Added forgotten file.
    svn path=/trunk/libavg/; revision=2539

---
+++ src/imaging/ICamera.cpp
@@ -0,0 +1,72 @@
+//
+//  libavg - Media Playback Engine. 
+//  Copyright (C) 2003-2006 Ulrich von Zadow
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  Current versions can be found at www.libavg.de
+//
+
+#include "ICamera.h"
+
+namespace avg {
+
+std::string cameraFeatureToString(CameraFeature Feature)
+{
+    switch(Feature) {
+        case CAM_FEATURE_BRIGHTNESS:
+            return "brightness";
+        case CAM_FEATURE_EXPOSURE:
+            return "exposure";
+        case CAM_FEATURE_SHARPNESS:
+            return "sharpness";
+        case CAM_FEATURE_WHITE_BALANCE:
+            return "white balance";
+        case CAM_FEATURE_HUE:
+            return "hue";
+        case CAM_FEATURE_SATURATION:
+            return "saturation";
+        case CAM_FEATURE_GAMMA:
+            return "gamma";
+        case CAM_FEATURE_SHUTTER:
+            return "shutter";
+        case CAM_FEATURE_GAIN:
+            return "gain";
+        case CAM_FEATURE_IRIS:
+            return "iris";
+        case CAM_FEATURE_FOCUS:
+            return "focus";
+        case CAM_FEATURE_TEMPERATURE:
+            return "temperature";
+        case CAM_FEATURE_TRIGGER:
+            return "trigger";
+        case CAM_FEATURE_ZOOM:
+            return "zoom";
+        case CAM_FEATURE_PAN:
+            return "pan";
+        case CAM_FEATURE_TILT:
+            return "tilt";
+        case CAM_FEATURE_OPTICAL_FILTER:
+            return "optical filter";
+        case CAM_FEATURE_CAPTURE_SIZE:
+            return "capture size";
+        case CAM_FEATURE_CAPTURE_QUALITY:
+            return "capture quality";
+        default:
+            return "unknown";
+    }
+}
+
+}
