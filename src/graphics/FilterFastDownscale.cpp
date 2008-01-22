736ec8832b3d231fa2623426ab0d50991b87fabc
uzadow <uzadow@libavg.de>
2008-01-22 16:24:33 +0000

    Optimized FilterFastDownscale.
    svn path=/trunk/libavg/; revision=2580

--- src/graphics/FilterFastDownscale.cpp
+++ src/graphics/FilterFastDownscale.cpp
@@ -52,16 +52,41 @@ BitmapPtr FilterFastDownscale::apply(BitmapPtr pBmpSrc)
     for (int y = 0; y<size.y; ++y) {
         unsigned char * pSrcPixel = pSrcLine;
         unsigned char * pDstPixel = pDestLine;
-        for (int x = 0; x < size.x; ++x) {
-            int DstPixel=0;
-            for (int y1 = 0; y1 < m_Factor; y1++) {
-                for (int x1 = 0; x1 < m_Factor; x1++) {
-                    DstPixel+= (int)(*(pSrcPixel+SrcStride*y1+x1));
+        switch (m_Factor) {
+            case 2:
+                for (int x = 0; x < size.x; ++x) {
+                    int DstPixel= int(*pSrcPixel)+int(*(pSrcPixel+1))
+                            +int(*(pSrcPixel+SrcStride))+int(*(pSrcPixel+SrcStride+1));
+                    *pDstPixel = (DstPixel+2)/4;
+                    pSrcPixel += 2;
+                    pDstPixel++;
                 }
-            }
-            *pDstPixel = (DstPixel+(m_Factor*m_Factor)/2)/(m_Factor*m_Factor);
-            pSrcPixel += m_Factor;
-            pDstPixel++;
+                break;
+            case 3:
+                for (int x = 0; x < size.x; ++x) {
+                    int DstPixel= int(*pSrcPixel)+int(*(pSrcPixel+1))+int(*(pSrcPixel+2))
+                            +int(*(pSrcPixel+SrcStride))+int(*(pSrcPixel+SrcStride+1))
+                                    +int(*(pSrcPixel+SrcStride+2))
+                            +int(*(pSrcPixel+SrcStride*2))+int(*(pSrcPixel+SrcStride*2+1))
+                                    +int(*(pSrcPixel+SrcStride*2+2));
+                    *pDstPixel = (DstPixel+4)/9;
+                    pSrcPixel += 3;
+                    pDstPixel++;
+                }
+                break;
+            default:
+                for (int x = 0; x < size.x; ++x) {
+                    int DstPixel=0;
+                    for (int y1 = 0; y1 < m_Factor; y1++) {
+                        for (int x1 = 0; x1 < m_Factor; x1++) {
+                            DstPixel+= (int)(*(pSrcPixel+SrcStride*y1+x1));
+                        }
+                    }
+                    *pDstPixel = (DstPixel+(m_Factor*m_Factor)/2)/(m_Factor*m_Factor);
+                    pSrcPixel += m_Factor;
+                    pDstPixel++;
+                }
+                break;
         }
         pSrcLine = pSrcLine + pBmpSrc->getStride()*m_Factor;
         pDestLine = pDestLine + pBmpDest->getStride();
