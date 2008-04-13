3e8a51738f1ecbb7db51018d71c183ec93bec3cb
uzadow <uzadow@libavg.de>
2008-04-13 14:16:06 +0000

    Audio branch: Fixed volume initialization.
    svn path=/branches/audio/; revision=2705

--- src/player/Video.cpp
+++ src/player/Video.cpp
@@ -72,7 +72,8 @@ Video::Video (const ArgList& Args, Player * pPlayer)
       m_FramesTooLate(0),
       m_FramesPlayed(0),
       m_bAudioEnabled(true),
-      m_pDecoder(0)
+      m_pDecoder(0),
+      m_Volume(1.0)
 {
     Args.setMembers(this);
     m_Filename = m_href;
