8ff798cae45786287d3ddafd0fe5b4c074e2f25d
mh <mh@libavg.de>
2008-08-13 16:56:53 +0000

    updated copyright notice (2006->2008)
    svn path=/trunk/libavg/; revision=3033

--- src/base/testbase.cpp
+++ src/base/testbase.cpp
@@ -1,6 +1,6 @@
 //
 //  libavg - Media Playback Engine.
-//  Copyright (C) 2003-2006 Ulrich von Zadow
+//  Copyright (C) 2003-2008 Ulrich von Zadow
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
