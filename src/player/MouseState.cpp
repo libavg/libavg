201d5c6bf92ab5712f3c79d26705cff08dd1afc9
uzadow <uzadow@libavg.de>
2008-08-20 10:33:13 +0000

    Added MouseState class to remember mouse events (and later drag start positions.
    svn path=/trunk/libavg/; revision=3048

---
+++ src/player/MouseState.cpp
@@ -0,0 +1,47 @@
+//
+//  libavg - Media Playback Engine. 
+//  Copyright (C) 2003-2008 Ulrich von Zadow
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  Current versions can be found at www.libavg.de
+//
+
+#include "MouseState.h"
+
+namespace avg {
+    MouseState::MouseState()
+        : m_pLastMouseEvent(new MouseEvent(Event::CURSORMOTION, false, false, false, 
+                IntPoint(0, 0), MouseEvent::NO_BUTTON))
+    {
+    }
+
+    MouseState::~MouseState()
+    {
+    }
+
+
+    MouseEventPtr MouseState::getLastEvent() const
+    {
+        return m_pLastMouseEvent;
+    }
+
+    void MouseState::setEvent(MouseEventPtr pEvent)
+    {
+        m_pLastMouseEvent = pEvent;
+    }
+    
+}
+
---
+++ src/player/MouseState.h
@@ -0,0 +1,47 @@
+//
+//  libavg - Media Playback Engine. 
+//  Copyright (C) 2003-2008 Ulrich von Zadow
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  Current versions can be found at www.libavg.de
+//
+
+#ifndef _MouseState_h_
+#define _MouseState_h_
+
+#include "MouseEvent.h"
+#include "Node.h"
+
+namespace avg {
+
+class MouseState {
+    public:
+        MouseState();
+        virtual ~MouseState();
+        
+        MouseEventPtr getLastEvent() const;
+        void setEvent(MouseEventPtr pEvent);
+//        IntPoint getDragStartPos(long button) const;
+
+    private:
+        MouseEventPtr m_pLastMouseEvent;
+//        IntPoint m_DragStartPos[3];
+};
+
+}
+
+#endif
+
