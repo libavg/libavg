a5948ebc5fb1702e09b4ae492c08de0513199fa8
uzadow <uzadow@libavg.de>
2008-09-26 16:53:04 +0000

    Fixed makedocs crash
    svn path=/trunk/libavg/; revision=3215

--- makedocs.sh
+++ makedocs.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-epydoc --html -v -o reference --name libavg --url ../index.php --docformat epytext --no-frames --graph classtree --inheritance grouped --css epydoc_libavg.css --no-private --graph-font="lucida console" libavg
+epydoc --html -vv -o reference --name libavg --exclude=avg_videoplayer --url ../index.php --docformat epytext --no-frames --graph classtree --inheritance grouped --css epydoc_libavg.css --no-private --graph-font="lucida console" libavg
