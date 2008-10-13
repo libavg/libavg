5aed573897345eb823b0f806806d488083ec34ad
regular <regular.gonzales@googlemail.com>
2008-10-13 19:47:13 +0000

    added build command for module loader
    svn path=/branches/libavg-plugin/; revision=3260

--- src/player/testplugin/build.sh
+++ src/player/testplugin/build.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+g++ loader.cpp -o loader
+
 gcc -c -fno-common HelloWorldPlugin.cpp -o HelloWorldPlugin.o
 gcc -bundle -flat_namespace -undefined suppress -o HelloWorldPlugin.so HelloWorldPlugin.o
 otool -hv HelloWorldPlugin.so
+chmod +x loader
+./loader
