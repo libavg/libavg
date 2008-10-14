87ecc5d9da23b435f9e84de3b15c5442d81c4d0f
regular <regular.gonzales@googlemail.com>
2008-10-14 20:48:34 +0000

    build script for linux now prpduces working code
    svn path=/branches/libavg-plugin/; revision=3265

--- src/player/testplugin/build-linux.sh
+++ src/player/testplugin/build-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-g++ -shared-libgcc -fPIC -ldl loader.cpp -o loader
+g++ -shared-libgcc -fPIC -export-dynamic -ldl loader.cpp -o loader
 
 gcc -fPIC -c -fno-common HelloWorldPlugin.cpp -o HelloWorldPlugin.o
 gcc -fPIC -shared -shared-libgcc -o HelloWorldPlugin.so HelloWorldPlugin.o
