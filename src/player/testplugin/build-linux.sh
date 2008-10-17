a57dd69724c976308b05235aa3f33fc92a166b4b
regular <regular.gonzales@googlemail.com>
2008-10-17 15:52:52 +0000

    XML parsing of plugged-in nodes works on linux
    svn path=/branches/libavg-plugin/; revision=3278

--- src/player/testplugin/build-linux.sh
+++ src/player/testplugin/build-linux.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #g++ -shared-libgcc -fPIC -export-dynamic -ldl loader.cpp Singleton.cpp -o loader
 
-gcc -fPIC -c -fno-common  -I. -I../../../src -I.. \
+g++ -fPIC -c -fno-common  -I. -I../../../src -I.. \
 -I/usr/include/libxml2 -I/usr/include/freetype2 \
 -I/usr/include/pango-1.0 -I/usr/include/freetype2 \
 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include \
 -I/usr/include/python2.5 -pthread -I/usr/include/ffmpeg \
 -I/usr/include/GraphicsMagick \
 ColorNode.cpp -o ColorNode.o
-gcc -fPIC -shared -shared-libgcc -o ColorNode.so ColorNode.o
+g++ -fPIC -shared -shared-libgcc -o ColorNode.so ColorNode.o  ../../wrapper/.libs/avg.so
+
+#gcc -fPIC -shared -o ColorNode.so ColorNode.o
 
 #chmod +x loader
 #./loader
