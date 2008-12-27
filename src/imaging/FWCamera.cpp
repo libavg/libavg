596611380b897a89511cd4404568789b24eabb00
uzadow <uzadow@libavg.de>
2008-12-27 00:13:51 +0000

    Fixed libdc1394 v1 compile.
    svn path=/trunk/libavg/; revision=3454

--- src/imaging/FWCamera.cpp
+++ src/imaging/FWCamera.cpp
@@ -538,13 +538,17 @@ void FWCamera::setFeature(dc1394feature_t Feature, int Value)
 
 void FWCamera::setFeatureOneShot(CameraFeature Feature)
 {
-#if defined(AVG_ENABLE_1394) || defined(AVG_ENABLE_1394_2)
     if (m_bCameraAvailable) {
+#if defined(AVG_ENABLE_1394) 
+        dc1394feature_t FeatureID = getFeatureID(Feature);
+        dc1394_auto_on_off(m_FWHandle, m_Camera.node, Feature, 0);
+        int err = dc1394_start_one_push_operation(m_FWHandle, m_Camera.node, FeatureID);
+#elif defined(AVG_ENABLE_1394_2)
         dc1394feature_t FeatureID = getFeatureID(Feature);
         dc1394error_t err = dc1394_feature_set_mode(m_pCamera, FeatureID, 
                 DC1394_FEATURE_MODE_ONE_PUSH_AUTO);
-    }
 #endif
+    }
 }
 
 #ifdef AVG_ENABLE_1394
