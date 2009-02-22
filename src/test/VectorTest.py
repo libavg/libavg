aebc0bb77f0a2be8038ed28aa808a23ca5285db9
uzadow <uzadow@libavg.de>
2009-02-22 10:14:43 +0000

    texturedvectors branch: More textured polyline tests.
    svn path=/branches/texturedvectors/; revision=3580

--- src/test/VectorTest.py
+++ src/test/VectorTest.py
@@ -209,13 +209,6 @@ class VectorTestCase(AVGTestCase):
         def testEmptyPolyline():
             polyline = canvas.getChild(0)
             polyline.pos=[]
-        def texturePolyLine():
-            canvas.removeChild(0)
-            canvas.removeChild(0)
-            polyline = Player.createNode("polyline", 
-                    {"strokewidth":20, "color":"FF00FF", "texhref":"rgb24-64x64.png"})
-            polyline.pos = [(10,10), (50,10), (90,50), (90, 90)]
-            canvas.appendChild(polyline)
         canvas = self.makeEmptyCanvas()
         self.start(None,
                 (addPolyLine,
@@ -227,9 +220,29 @@ class VectorTestCase(AVGTestCase):
                  addPolyLine2,
                  lambda: self.compareImage("testPolyLine4", False),
                  testEmptyPolyline,
-                 lambda: self.compareImage("testPolyLine5", False),
-                 texturePolyLine,
-                 lambda: self.compareImage("testPolyLine6", False)
+                 lambda: self.compareImage("testPolyLine5", False)
+                ))
+
+    def testTexturedPolyLine(self):
+        def texturePolyLine():
+            polyline = Player.createNode("polyline", 
+                    {"strokewidth":20, "color":"FF00FF", "texhref":"rgb24-64x64.png"})
+            polyline.pos = [(10,10), (50,10), (90,50), (90, 90)]
+            canvas.appendChild(polyline)
+        def miter():
+            polyline = canvas.getChild(0)
+            polyline.linejoin = "miter"
+        def setTexCoords():
+            polyline = canvas.getChild(0)
+            polyline.texcoords = [-1, 0, 1, 2]
+        canvas = self.makeEmptyCanvas()
+        self.start(None,
+                (texturePolyLine,
+                 lambda: self.compareImage("testTexturedPolyLine1", False),
+                 miter,
+                 lambda: self.compareImage("testTexturedPolyLine2", False),
+                 setTexCoords,
+                 lambda: self.compareImage("testTexturedPolyLine3", False)
                 ))
 
     def testPolygon(self):
@@ -293,6 +306,7 @@ def vectorTestSuite(tests):
             "testRect",
             "testCurve",
             "testPolyLine",
+            "testTexturedPolyLine",
             "testPolygon",
             )
     return AVGTestSuite (availableTests, VectorTestCase, tests)
