6dd501300ff78365768f5ccbf6edced859c0be1b
uzadow <uzadow@libavg.de>
2009-04-12 18:35:08 +0000

    Reverted bogus checkin 3756.
    svn path=/trunk/libavg/; revision=3757

--- src/player/VectorNode.cpp
+++ src/player/VectorNode.cpp
@@ -66,8 +66,6 @@ VectorNode::VectorNode(const ArgList& Args)
     setTexHRef(m_TexHRef);
     m_sColorName = Args.getArgVal<string>("color");
     m_Color = colorStringToColor(m_sColorName);
-    ObjectCounter::get()->incRef(&typeid(*this));
-    
 }
 
 VectorNode::~VectorNode()
