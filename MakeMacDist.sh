56a951c32ccd629a1a0be1c1d96add648d9d21e1
uzadow <uzadow@libavg.de>
2009-08-09 22:14:18 +0000

    Mac installer update.
    svn path=/trunk/libavg/; revision=4049

--- MakeMacDist.sh
+++ MakeMacDist.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-export VERSION=0.8.0
+export VERSION=0.9.0
 export INSTALL_PATH_10_4="/Library/Python/2.3/site-packages/libavg"
 export INSTALL_PATH_10_5="/Library/Python/2.5/site-packages/libavg"
 
@@ -37,7 +37,7 @@ makeOneDist()
     mkdir avg/video
     mkdir avg/video/testfiles
     cp -Rv /Library/Python/$PYTHON_VER/site-packages/libavg/ .
-    strip -S avg.0.0.0.so
+    strip -S avg.0.so
     cp ../../libavg/src/avgrc avg
     mkdir etc
     cp -R /etc/fonts etc/
