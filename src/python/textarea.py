e145dc5a287782faa693abe5b1f3d3b625ce706e
uzadow <uzadow@libavg.de>
2009-08-11 22:33:04 +0000

    Reverted last checkin because it broke the build.
    svn path=/trunk/libavg/; revision=4064

--- src/python/textarea.py
+++ src/python/textarea.py
@@ -70,9 +70,9 @@ DEFAULT_BLUR_OPACITY = 0.3
 
 import time
 try:
-    from . import avg, Point2D
+    from . import avg
 except ValueError:
-    from libavg import Point2D
+    pass
 
 
 class FocusContext:
@@ -367,8 +367,8 @@ class TextArea:
                 w = 1
             self.__cursorNode.strokewidth = w
         x  = self.__cursorNode.strokewidth / 2.0
-        self.__cursorNode.pos1 = Point2D(x, self.__cursorNode.pos1.y)
-        self.__cursorNode.pos2 = Point2D(x, self.__cursorNode.pos2.y)
+        self.__cursorNode.pos1.x = x
+        self.__cursorNode.pos2.x = x
         
         self.__flashingCursor = flashingCursor
         if not flashingCursor:
@@ -528,14 +528,14 @@ class TextArea:
                 lastCharExtents = (0, lastCharExtents[1])
         
         if lastCharExtents[1] > 0:
-            self.__cursorNode.pos2 = Point2D(0, lastCharExtents[1] * (1 - CURSOR_PADDING_PCT/100.0))
+            self.__cursorNode.pos2.y = lastCharExtents[1] * (1 - CURSOR_PADDING_PCT/100.0)
         else:
-            self.__cursorNode.pos2 = Point2D(0, self.__textNode.fontsize)
+            self.__cursorNode.pos2.y = self.__textNode.fontsize
         
         self.__cursorContainer.x = lastCharPos[0] + lastCharExtents[0] + self.__border
         self.__cursorContainer.y = (lastCharPos[1] +
             self.__cursorNode.pos2.y * CURSOR_PADDING_PCT/200.0 + self.__border)
-        
+    
     def __updateLastActivity(self):
         self.__lastActivity = time.time()
         
