bc57d176a22282c5b2843a65437e5589427b427f
sm <sm@libavg.de>
2009-11-16 12:45:25 +0000

    mtemu: activated when shift is pressed.
    svn path=/trunk/libavg/; revision=4302

---
+++ src/python/AVGAppStarterHelp.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# libavg - Media Playback Engine.
+# Copyright (C) 2003-2008 Ulrich von Zadow
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Current versions can be found at www.libavg.de
+#
+# Original author of this file is Sebastian Maulbeck 
+# <sm (at) archimedes-solutions (dot) de>
+
+"""
+this class is for displaying all active keys. 
+
+        '#'     -->      on/off. 
+"""
+
+from libavg import avg
+
+g_player = avg.Player.get()
+
+class MThelp():
+
+    def __init__(self, appStarter):
+        self.__appstarter = appStarter
+        self.__stackOfBackups = []      
+        self.__rectNode = g_player.createNode("""
+            <div id="RectNode" opacity="0" sensitive="False" x="5" y="10" size="(450,450)"> 
+                <rect id="rectWhite" strokewidth="0" fillopacity="0.1" 
+                        fillcolor="FFFFFF" pos="(5,5)" size="(450,450)"/>
+                <rect id="rectBlack" strokewidth="0" fillopacity="0.6" 
+                        fillcolor="000000" pos="(6,6)" size="(451,451)"/>
+            </div>
+        """)
+        self.__keysNode = g_player.createNode("""
+                    <words x="10" y="10" fontsize="18" opacity="1" 
+                            color="DDDDDD" text=""/>
+            """)
+        rootNode = g_player.getRootNode()
+        rootNode.appendChild(self.__rectNode)
+        self.__rectNode.appendChild(self.__keysNode)
+       
+    def showHelp(self):
+        show = self.__appstarter.showingHelp
+        self.__keyBindDown = self.__appstarter.getKeyDowns()
+        TextHelp = ''   
+        if show == True:
+            for key in sorted(self.__keyBindDown.iterkeys()):
+                funcName = self.__keyBindDown[key][1]
+                TextHelp = TextHelp + \
+                    "<span><b>%s</b>     <small>%s</small><br/></span>"% (key, funcName)
+            self.__keysNode.text = TextHelp
+            self.__rectNode.opacity = 1
+            g_player.getElementByID('rectWhite').size = self.__keysNode.getMediaSize()
+            g_player.getElementByID('rectBlack').size = self.__keysNode.getMediaSize()
+            
+            self.__rectNode.getParent().reorderChild(
+                    self.__rectNode.getParent().indexOf(self.__rectNode), 
+                    self.__rectNode.getParent().getNumChildren()-1) 
+            
+        elif show == False:
+            self.__keysNode.text = ''
+            self.__rectNode.opacity = 0
+            
+    def backupKeys(self):
+        # backup keybindings if you change active app.
+        # keys will be stored in a dict. keybindings from new function can be loaded
+        # and conflicts by double keybindings are reduced to only the active app.
+        
+        self.__appstarter.showingHelp = False
+        self.showHelp()
+        self.__keyBindDown = self.__appstarter.getKeyDowns()
+        self.__keyBindUp = self.__appstarter.getKeyUps()
+        
+        self.__stackOfBackups.append(self.__keyBindDown)
+        self.__stackOfBackups.append(self.__keyBindUp)
+        self.__appstarter.setKeyDowns({})
+        self.__appstarter.setKeyUps({}) 
+        self.__appstarter.bindKey('#', self.__appstarter.activateHelp, 'HELP')
+      
+    def restoreKeys(self):
+        # restore keybindings if last active app will be active again.
+        # stored keys will be restored in the keybindings.
+        
+        self.__appstarter.showingHelp = False
+        self.showHelp()  
+        self.__keyBindUp = self.__appstarter.setKeyUps(self.__stackOfBackups.pop())
+        self.__keyBindDown = self.__appstarter.setKeyDowns(self.__stackOfBackups.pop())
--- src/python/mtemu.py
+++ src/python/mtemu.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009 Archimedes Solutions GmbH,
-# Saarbrücker Str. 24b, Berlin, Germany
+# libavg - Media Playback Engine.
+# Copyright (C) 2003-2008 Ulrich von Zadow
 #
-# This file contains proprietary source code and confidential
-# information. Its contents may not be disclosed or distributed to
-# third parties unless prior specific permission by Archimedes
-# Solutions GmbH, Berlin, Germany is obtained in writing. This applies
-# to copies made in any form and using any medium. It applies to
-# partial as well as complete copies.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Current versions can be found at www.libavg.de
+#
+# Original author of this file is Sebastian Maulbeck 
+# <sm (at) archimedes-solutions (dot) de>
+
 
 """
 this class provides a test to emulate one or two TOUCH/TRACK events. 
-by pressing "ctrl left" the TOUCH events will be switched into TRACK events and the other way around.
-first TOUCH/TRACK should be created with a left mouse button. after that you can create another by 
-pressing the right button. 
-the second event appears at the position:   mousePosition - (50,50) pixels.
+by pressing "ctrl left/right" the TOUCH events will be switched into TRACK events and the 
+other way around.
+by pressing "shift left/right" a second event is created whenever the mousebutton (left) 
+is clicked. 
+the second event appears at the position:   mousePosition - (40,40) pixels.
 
 Note: remove any mouse event handling from your application to avoid emulation issues
 
@@ -24,10 +38,12 @@ For example:
 
 1) avg.MOUSE | avg.TOUCH filters can be safely reduced to avg.TOUCH
 2) the current libavg button module breaks the emulation
-3) the current libavg Grabbable class breaks the emulation, unless is created with source = avg.TOUCH parameter
+3) the current libavg Grabbable class breaks the emulation, unless is created with 
+   source = avg.TOUCH parameter
 """
 
 from libavg import avg, Point2D
+import warnings
 
 Player = avg.Player.get()
 
@@ -37,25 +53,29 @@ class MTemu(object):
     emulate one or two TOUCH events. 
     by pressing "ctrl left" the TOUCH events 
     will be switched into TRACK events.
-    
-    ID behaviour:
-    left clicks have ID up from 10000
-    right clicks havve ID up from 0
-    so in between 10000 clicks it won't be a problem.
     """
     
     ID = Point2D(10000,0)
     EventMode = avg.TOUCH
-    pos1 = pos2 = (-1,-1)
-    mouseDown1 = mouseDown2 = (-1,-1)
+    pos1 = pos2 = Point2D(-1,-1)
+    mouseDown1 = mouseDown2 = Point2D(-1,-1)
     hover = False  
+    multiActive = False
+    mouseState = ''
     
     def __init__(self):
-        """
-        @param rootNode: this function is called with
-        the rootnode of the application.
-        """
         rootNode = Player.getRootNode()
+        posX = rootNode.size.x * 3/4
+        posY = rootNode.size.y-40
+        
+        self.__layer = Player.createNode(''' 
+                <words id="displayEmu" x="%(posX)i" y="%(posY)i" fontsize="20" opacity="1" 
+                color="DDDDDD" text="multitouch emulation active!" sensitive="False" />
+                '''
+                % {'posX':posX, 'posY':posY} 
+                )
+        rootNode.appendChild(self.__layer)  
+                   
         self.__container = Player.createNode('div', {})
         self.__p1 = Player.createNode('circle', {'r': 10, 'fillcolor':'ff0000',
             'fillopacity':0, 'opacity':0, 'sensitive':False})
@@ -67,241 +87,214 @@ class MTemu(object):
         self.__container.appendChild(self.__p2)
           
         rootNode.setEventHandler(avg.CURSORMOTION, avg.MOUSE, self.__onMouseMove)
-        rootNode.setEventHandler(avg.CURSORDOWN, avg.MOUSE, self.__mouseButtonDown)
-        rootNode.setEventHandler(avg.CURSORUP, avg.MOUSE, self.__mouseButtonUp)
-            
+        rootNode.setEventHandler(avg.CURSORDOWN, avg.MOUSE, self.__onMouseButtonDown)
+        rootNode.setEventHandler(avg.CURSORUP, avg.MOUSE, self.__onMouseButtonUp)
+        
+                    
     def delete(self): 
         """
         after turning off the emulater the function unlinks
         all nodes of the emulator. so it be garanted that the 
         application is working as before again.
+        events will be cleared with two UP events.
         """
         rootNode = Player.getRootNode()
-
         rootNode.setEventHandler(avg.CURSORMOTION, avg.MOUSE, None)
         rootNode.setEventHandler(avg.CURSORDOWN, avg.MOUSE, None)
         rootNode.setEventHandler(avg.CURSORUP, avg.MOUSE, None)
-        
+        self.__layer.unlink()
         self.__container.unlink()
         self.__p1.unlink()
         self.__p2.unlink()
+        self.__p1 = self.__p2 = None
+        self.__container = self.__node2root = self.__layer = None
+        Player.getTestHelper().fakeTouchEvent(self.__getLeftID(), avg.CURSORUP,
+                    self.EventMode, self.pos1, self.pos1, Point2D(0,0))
+        Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORUP,
+                    self.EventMode, self.pos2, self.pos2, Point2D(0,0))
         
-        self.__p1 = self.__p2 = self.__container = self.__node2root = None
-        
-    def _leftIDup(self):
-        """
-        ID for a left Click will be increased.
-        """
+    def __leftIDup(self):
         self.ID = self.ID + (1,0)
         
-    def _getLeftID(self):
-        """
-        returns the current ID for left events (TOUCH or TRACK)
-        """
+    def __getLeftID(self):
         self.idpos1 = int(self.ID.x)
         return self.idpos1
         
-    def _rightIDup(self):
-        """
-        ID for a right Click will be increased.
-        """
+    def __rightIDup(self):
         self.ID = self.ID + (0,1)
          
-    def _getRightID(self):
-        """
-        returns the current ID for right events (TOUCH or TRACK)
-        """
+    def __getRightID(self):
         self.idpos2 = int(self.ID.y)
         return self.idpos2
         
         
-    def __mouseButtonDown(self,e):
-        """
-        activated if mousebutton is pressed (left or right press).
-        a fake event will be created. 
-        what kind of fake event (touch or track) depends on the EventMode. 
-        by pressing 'ctrl left' the EventMode switches between TRACK
-        and TOUCH.
-        (default: TOUCH)
-        
-        when two events should be created it works better if you create first
-        the left event and then the right event. so the handling of the two TOUCHES
-        with only one mouse is better. 
-        """ 
+    def __onMouseButtonDown(self,e):
+        #activation with a left mouse click.
+        #one or two fake UP events will be created (multitouch active or not). 
+        #SOURCE depending on EventMode. 
         
         if e.button == 1:
+            self.mouseState = 'Down'
             self.pos1 = e.pos
             self.mouseDown1 = self.pos1
-            self.__p1.fillopacity = 1
-            Player.getTestHelper().fakeTouchEvent(self._getLeftID(), avg.CURSORDOWN,
-                    self.EventMode, self.pos1, self.pos1, Point2D(0,0)) 
-        elif e.button == 2:
-            self.pos2 = e.pos - Point2D(50, 50)
-            self.mouseDown2 = self.pos2
-            self.__p2.fillopacity = 1
-            Player.getTestHelper().fakeTouchEvent(self._getRightID(), avg.CURSORDOWN,
-                    self.EventMode, self.pos2, self.pos2, Point2D(0,0))
-        
-        if self.EventMode == avg.TOUCH:
-            self.drawTouch(e)
-        else:
-            self.drawTrack(e)
+            self.__leftIDup()
+            Player.getTestHelper().fakeTouchEvent(self.__getLeftID(), avg.CURSORDOWN,
+                        self.EventMode, self.pos1, self.pos1, Point2D(0,0))
+            
+            if self.multiActive:
+                
+                self.mouseDown2 = e.pos - Point2D(20,20)
+                self.pos2 = 2*Point2D(self.mouseDown2) - Point2D(20,20) - e.pos
+                self.__rightIDup()
+                Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORDOWN,
+                        self.EventMode, self.pos2, self.pos2, Point2D(0,0))
+                            
+            self.__drawTouch()
         return True
-  
+
     def __onMouseMove(self, e):
-        """
-        is activated if one or two mouse buttons are pressed AND moved. 
-        If the left and the right mouse button is pressed it creates 
-        two fake events which are handled in the same way. what kind 
-        of fake event (TOUCH/TRACK) depends on the EventMode. 
-        by pressing 'ctrl left' the EventMode switches between TRACK
-        and TOUCH.
-        (default: TOUCH)
+        #is activated if left mouse button is pressed AND moved. 
         
-        left or right button pressed:
-        TOUCH/TRACK will be created which gets the position of the mouse.
+        #If the left mouse button is pressed and multitouch (shift) 
+        #is acitvated it creates two fake events. 
+        #One event position will be set to the mouse position. 
+        #the second event will move in relation to the mouse position. 
         
-        left and right button pressed:
-        two TRACK/TOUCH events will be created. the motion of the two events
-        will be controlled by the mouse and a specific function.
-        """
-       
-        if (e.leftbuttonstate and e.rightbuttonstate) or e.middlebuttonstate:
+        if e.leftbuttonstate and self.multiActive:
             self.pos1 = e.pos 
-            self.pos2 = 2*e.lastdownpos - Point2D(50, 50) - e.pos
-            Player.getTestHelper().fakeTouchEvent(self._getLeftID(), avg.CURSORMOTION,
+            self.pos2 = 2*Point2D(self.mouseDown2) - Point2D(20,20) - e.pos
+            Player.getTestHelper().fakeTouchEvent(self.__getLeftID(), avg.CURSORMOTION,
                     self.EventMode, self.pos1, self.pos1, e.speed)
-            Player.getTestHelper().fakeTouchEvent(self._getRightID(), avg.CURSORMOTION,
-                    self.EventMode, self.pos2, self.pos2, e.speed)
-        
-        elif e.leftbuttonstate and not e.rightbuttonstate:
+            Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORMOTION,
+                    self.EventMode, self.pos2, self.pos2, -e.speed)
+            self.__drawTouch()
+            
+        elif e.leftbuttonstate and not self.multiActive:
             self.pos1 = e.pos
-            Player.getTestHelper().fakeTouchEvent(self._getLeftID(), avg.CURSORMOTION,
+            Player.getTestHelper().fakeTouchEvent(self.__getLeftID(), avg.CURSORMOTION,
                     self.EventMode, self.pos1, self.pos1, e.speed)
-            
-        elif e.rightbuttonstate and not e.leftbuttonstate:
-            self.pos2 = e.pos
-            Player.getTestHelper().fakeTouchEvent(self._getRightID(), avg.CURSORMOTION,
-                    self.EventMode, self.pos2, self.pos2, e.speed)
+            self.__drawTouch()
          
-        if self.EventMode == avg.TOUCH:
-            self.drawTouch(e)
-        else:
-            self.drawTrack(e)
-        
         return True
        
        
-       
-    def __mouseButtonUp(self, e):
-        """
-        fake UP events will be created for the TOUCH or TRACK emulation.
-        ID will be increased. 
-        if event comes from left button:
-        ID of a left event will be set to CURSORUP
-        
-        if event comes from right button:
-        ID of a right event will be set to CURSORUP
-        
-        if event comes from both (button 3 = middlebutton)
-        both actual events with their ID will be set to CURSORUP 
-        """
-        self.eraseDraw(e)
-  
-        if e.button == 3:
-            Player.getTestHelper().fakeTouchEvent(self._getLeftID(), avg.CURSORUP,
-                    self.EventMode, self.pos1, self.pos1, Point2D(0,0))
-            Player.getTestHelper().fakeTouchEvent(self._getRightID(), avg.CURSORUP,
-                    self.EventMode, self.pos2, self.pos2, Point2D(0,0))
-            self.pos1 = (-1,-1)
-            self.mouseDown1 = (-1,-1)
-            self.pos2 = (-1,-1)
-            self.mouseDown2 = (-1,-1)
-            self._leftIDup()
-            self._rightIDup()
-        
-
-        elif e.button == 1:
-            Player.getTestHelper().fakeTouchEvent(self._getLeftID(), avg.CURSORUP,
-                    self.EventMode, self.pos1, self.pos1, Point2D(0,0))
-            self.pos1 = (-1,-1)
-            self.mouseDown1 = (-1,-1)
-            self._leftIDup()
-
-        elif e.button == 2:
-            Player.getTestHelper().fakeTouchEvent(self._getRightID(), avg.CURSORUP,
-                    self.EventMode, self.pos2, self.pos2, Point2D(0,0))
-            self.pos2 = (-1,-1)
-            self.mouseDown2 = (-1,-1)
-            self._rightIDup()
-        
-        return True
+    def __onMouseButtonUp(self, e):
+        #one or two fake UP events will be created (multitouch active or not). 
+        #SOURCE depending on EventMode. 
             
-            
-    def drawTouch(self,e):
-        """
-        function to draw the fake TOUCH events.
-        
-        color:    red (ff0000)
-        radius:   6
-        opacity:  1
-        """
-        self.__p1.pos = self.pos1
-        self.__p1.r = 6
-        self.__p1.fillcolor = 'ff0000'
-        
-        self.__p2.pos = self.pos2
-        self.__p2.r = 6 
-        self.__p2.fillcolor = 'ff0000'
-        
-    def eraseDraw(self,e):
-        """
-        function to erase the fake TOUCH events.
-        opacity = 0
-        """
+        self.__eraseDraw()  
         if e.button == 1:
-            self.__p1.fillopacity = 0
-        elif e.button == 2:
-            self.__p2.fillopacity = 0
-        elif e.button == 3:
-            self.__p1.fillopacity = 0
-            self.__p2.fillopacity = 0
-    
-    def drawTrack(self,e):
-        """
-        function to erase the fake TRACK events. opacity 
-        and radius will be changed to get a hover similar 
-        appearance.
+            self.mouseState = 'Up'
+            Player.getTestHelper().fakeTouchEvent(self.__getLeftID(), avg.CURSORUP,
+                    self.EventMode, self.pos1, self.pos1, Point2D(0,0))
+            self.pos1 = (-1,-1)
+            self.mouseDown1 = (-1,-1)
+
+            
+            if self.multiActive:
+                Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORUP,
+                        self.EventMode, self.pos2, self.pos2, Point2D(0,0))
+                self.pos2 = (-1,-1)
+                self.mouseDown2 = (-1,-1)
+
+        return True
+            
+            
+    def __drawTouch(self):
+        #function to draw the fake events. style depending on EventMode.
+        if self.EventMode == avg.TOUCH:
+            self.__p1.pos = self.pos1
+            self.__p1.r = 6
+            self.__p1.fillcolor = 'ff0000'
+            self.__p1.fillopacity = 1
+            
+            self.__p2.pos = self.pos2
+            self.__p2.r = 6 
+            self.__p2.fillcolor = 'ff0000'
+            if self.multiActive:
+                self.__p2.fillopacity = 1
+            else:
+                self.__p2.fillopacity = 0
+        else:
+            self.__p1.r = 13
+            self.__p1.pos = self.pos1
+            self.__p1.fillcolor = 'ffffff'
+            self.__p1.fillopacity = 0.4
+            
+            self.__p2.r = 13
+            self.__p2.pos = self.pos2
+            self.__p2.fillcolor = 'ffffff'
+            if self.multiActive:
+                self.__p2.fillopacity = 0.4
+            else:
+                self.__p2.fillopacity = 0
         
-        color:    white (ffffff)
-        radius:   13
-        opacity:  0.4
         
-        """
-        self.__p1.r = 13
-        self.__p2.r = 13
-        self.__p1.pos = self.pos1
-        self.__p2.pos = self.pos2
-        self.__p1.fillopacity = 0.4
-        self.__p1.fillcolor = 'ffffff'
-        self.__p2.fillopacity = 0.4
-        self.__p2.fillcolor = 'ffffff'
-    
+    def __eraseDraw(self):
+        #function to erase the fake TOUCH events.
+        
+        self.__p1.fillopacity = 0
+        if self.multiActive:
+            self.__p2.fillopacity = 0
+
     def changeMode(self):
         """
         function to switch between TRACK and TOUCH.
         it is called from the AVGAppStarter by pressing
         'ctrl left'.
         """ 
+        
+        self.__eraseDraw()
+        Player.getTestHelper().fakeTouchEvent(self.__getLeftID(),  avg.CURSORUP,
+                self.EventMode, self.pos1, self.pos1, Point2D(0,0))
+
+        if self.multiActive:
+            Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORUP,
+                    self.EventMode, self.pos2, self.pos2, Point2D(0,0))
+   
         self.hover = not self.hover
-        Player.getTestHelper().fakeTouchEvent(self._getLeftID(),  avg.CURSORUP,
-                self.EventMode, self.pos1, self.pos1, Point2D(0,0))
-        Player.getTestHelper().fakeTouchEvent(self._getRightID(), avg.CURSORUP,
-                self.EventMode, self.pos1, self.pos1, Point2D(0,0))
         if self.hover == True:
             self.EventMode = avg.TRACK
         else:
             self.EventMode = avg.TOUCH            
-
             
-    
+        if self.mouseState == 'Down':     
+            self.mouseDown1 = self.pos1  
+            self.__leftIDup()
+            Player.getTestHelper().fakeTouchEvent(self.__getLeftID(),  avg.CURSORDOWN,
+                    self.EventMode, self.pos1, self.pos1, Point2D(0,0))
+            if self.multiActive:
+                e = Player.getMouseState()
+                self.mouseDown2 = e.pos - Point2D(20,20)
+                self.pos2 = 2*Point2D(self.mouseDown2) - Point2D(20,20) - e.pos
+                self.__rightIDup()
+                Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORDOWN,
+                        self.EventMode, self.pos2, self.pos2, Point2D(0,0))
+            self.__drawTouch()
+        
+    def multiTouch(self):
+        """
+        creates another event if shift and mouse left is pressed.
+        if the mouse is not pressed no event will appear.
+        """
+        self.multiActive = not self.multiActive  
+         
+        if self.mouseState == 'Down': 
+            self.__eraseDraw()
+            
+            if not self.multiActive:
+                Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORUP,
+                        self.EventMode, self.pos2, self.pos2, Point2D(0,0))
+            
+            if self.multiActive:
+                e = Player.getMouseState()
+                self.mouseDown2 = e.pos - Point2D(20,20)
+                self.pos2 = 2*Point2D(self.mouseDown2) - Point2D(20,20) - e.pos 
+                self.__rightIDup() 
+                Player.getTestHelper().fakeTouchEvent(self.__getRightID(), avg.CURSORDOWN,
+                        self.EventMode, self.pos2, self.pos2, Point2D(0,0))
+            
+            self.__drawTouch()
+            
+        return True
+        
