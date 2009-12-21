81c1384923775c6998cf876be88242a36cffd6e0
uzadow <uzadow@libavg.de>
2009-12-21 12:06:53 +0000

    Updated UnpackDeps.sh to new mac dependencies.
    svn path=/trunk/libavg/; revision=4385

--- mac/UnpackDeps.sh
+++ mac/UnpackDeps.sh
@@ -27,11 +27,8 @@ cd fontconfig-2.7.0
 patch -R Makefile.am <../../libavg/mac/fontconfig-disablecache.patch
 patch fontconfig.pc.in < ../../libavg/mac/fontconfig.pc.in.patch
 cd ..
-cd ffmpeg/libswscale
-patch -p0 <../../../libavg/mac/ffmpeg.broken-yuv.patch
+cd glib-2.21.3/glib
+patch -R gconvert.c < ../../../libavg/mac/glib.patch
 cd ../..
-cd SDL-1.2.13/src/video/quartz
-patch -p0 < ../../../../../libavg/mac/SDL_QuartzVideo.patch
-cd ../../../..
 
 echo "Done"
