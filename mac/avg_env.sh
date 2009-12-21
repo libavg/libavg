27240420f984143ee7fc9e213906fa7e4306e6c1
uzadow <uzadow@libavg.de>
2009-12-21 20:52:59 +0000

    Fixed mac PYTHONPATH
    svn path=/trunk/libavg/; revision=4391

--- mac/avg_env.sh
+++ mac/avg_env.sh
@@ -8,6 +8,12 @@ else
     export CFLAGS="-O3 -g -Wall -pipe "$CFLAGS
     export LDFLAGS="-L${AVG_PATH}/lib "$LDFLAGS
     export PKG_CONFIG_PATH=${AVG_PATH}/lib/pkgconfig
-    export PYTHONPATH=${AVG_PATH}/lib/python/2.5/site-packages/:$PYTHONPATH
+    DARWINVER=`uname -r`
+    DARWINMAJORVER=${DARWINVER%%.*}
+    if [[ "${DARWINMAJORVER}" == "10" ]]
+    then
+        export PYTHONPATH=${AVG_PATH}/lib/python2.6/site-packages/:$PYTHONPATH
+    else
+        export PYTHONPATH=${AVG_PATH}/lib/python/2.5/site-packages/:$PYTHONPATH
     export AVG_MAC_ENV_SET=1
 fi
