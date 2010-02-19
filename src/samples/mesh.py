e373ff945db71148e2c932aba2386e98e0898f99
uzadow <uzadow@libavg.de>
2010-02-19 10:40:49 +0000

    Added example for mesh node.
    svn path=/trunk/libavg/; revision=4613

---
+++ src/samples/mesh.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from libavg import avg
+
+player = avg.Player.get()
+
+player.loadString("""
+<avg width="160" height="120"/>
+""")
+mesh = player.createNode("mesh", {"texhref":"rgb24-64x64.png",
+        "vertexcoords":((0,0), (64,0), (0,64), (64, 64), (32, 32)),
+        "texcoords":((0,0), (1,0), (0,1), (1,1), (0.3,0.5)),
+        "triangles":((0,1,4), (1,3,4), (3,2,4), (2,0,4))
+    })
+player.getRootNode().appendChild(mesh)
+player.setResolution(0,640,0,0)
+player.play()
+
