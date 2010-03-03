4ec910dfa1c4f3c01a44f8caadd300a858e7803a
oxullo <x@brainrapers.org>
2010-03-03 18:20:29 +0000

    Test fixes for distcheck, fix for PluginTest
    svn path=/trunk/libavg/; revision=4668

--- src/test/Test.py
+++ src/test/Test.py
@@ -23,7 +23,8 @@
 '''
 Entry point for libavg unit tests
 On autotools-based systems, tests are performed on a local libavg package.
-This package is created by symlinking avg.so and a package container.
+This package is created by copying all the relevant files in a local, temporary
+directory, and its scope is added in order to let python to find it.
 On windows instead, tests are carried on after distutils takes care of a
 system-wide installation.
 '''
@@ -33,20 +34,45 @@ from optparse import OptionParser, OptionValueError
 import sys
 import os
 import platform
+import shutil
 
-def setSymlink(src, dest):
-    if not os.path.exists(dest):
-        os.symlink(src, dest)
-    elif not os.path.islink(dest):
-        raise RuntimeError(
-            '%s exists as a file/directory. Please remove it to perform tests' % dest)
-    
+g_TempPackageDir = None
+
+def cleanExit(rc):
+    if g_TempPackageDir is not None:
+        try:
+            shutil.rmtree(g_TempPackageDir)
+        except OSError:
+            print 'ERROR: Cannot clean up test package directory'
+
+    sys.exit(rc)
+
 if platform.system() != 'Windows':
-    sys.path.insert(0, '..')
-    setSymlink('python', '../libavg')
-    setSymlink('../wrapper/.libs/avg.so', '../libavg/avg.so')
-    setSymlink('../wrapper/__init__.py', '../libavg/__init__.py')
+    print 'Creating local libavg python package'
+    if os.getenv('srcdir') in ('.', None):
+        # We're running make check
+        shutil.copytree('../python', 'libavg')
+        shutil.copy('../wrapper/__init__.py', 'libavg/')
+    else:
+        # make distcheck
+        shutil.copytree('../../../../src/python', 'libavg')
+        shutil.copy('../../../../src/wrapper/__init__.py', 'libavg/')
+        sys.path.insert(0, os.getcwd())
+    
+    shutil.copy('../wrapper/.libs/avg.so', 'libavg/')
+    g_TempPackageDir = os.path.join(os.getcwd(), 'libavg')
 
+    # The following lines help to prevent the test to be run
+    # with an unknown version of libavg, which can be laying somewhere
+    # in the system
+    import libavg
+
+    cpfx = os.path.commonprefix((libavg.__file__, os.getcwd()))
+    
+    if cpfx != os.getcwd():
+        raise RuntimeError('Tests would be performed with a non-local libavg package (%s)'
+            % libavg.__file__)
+    
 from libavg import avg
 
 SrcDir = os.getenv("srcdir",".")
@@ -87,83 +113,91 @@ def dumpConfig():
     Log.popCategories()
    
 
-if os.getenv("AVG_CONSOLE_TEST"):
-    runConsoleTest()
-else:
-    def setPow2(option, opt, value, parser):
-        if value not in ('yes','no'):
-            raise OptionValueError('argument must be "yes" or "no"')
-        setUsePOW2Textures(value == 'yes')
-    def setShaders(option, opt, value, parser):
-        if value not in ('yes','no'):
-            raise OptionValueError('argument must be "yes" or "no"')
-        setUseShaders(value == 'yes')
-    def setPixBuf(option, opt, value, parser):
-        if value not in ('yes','no'):
-            raise OptionValueError('argument must be "yes" or "no"')
-        setUsePixelBuffers(value == 'yes')
+def main():
+    if os.getenv("AVG_CONSOLE_TEST"):
+        runConsoleTest()
+    else:
+        def setPow2(option, opt, value, parser):
+            if value not in ('yes','no'):
+                raise OptionValueError('argument must be "yes" or "no"')
+            setUsePOW2Textures(value == 'yes')
+        def setShaders(option, opt, value, parser):
+            if value not in ('yes','no'):
+                raise OptionValueError('argument must be "yes" or "no"')
+            setUseShaders(value == 'yes')
+        def setPixBuf(option, opt, value, parser):
+            if value not in ('yes','no'):
+                raise OptionValueError('argument must be "yes" or "no"')
+            setUsePixelBuffers(value == 'yes')
 
-    parser = OptionParser("usage: %prog [options] [<suite> [testcase] [testcase] [...]]")
-    parser.add_option("-b", "--bpp", dest = "bpp",
-            type = "int",
-            help = "set pixel depth")
-    parser.add_option("-2", "--usepow2textures", dest = "usepow2textures",
-            type = "string",
-            action = 'callback', callback = setPow2,
-            help = "Use power of 2 textures (yes, no)")
-    parser.add_option("-s", "--useshaders", dest = "shaders",
-            type = "string",
-            action = 'callback', callback = setShaders,
-            help = "Use shaders (yes, no)")
-    parser.add_option("-p", "--usepixelbuffers", dest = "usepixelbuffers",
-            type = "string",
-            action = 'callback', callback = setPixBuf,
-            help = "Use pixel buffers (yes, no)")
-    parser.set_defaults(bpp = 24)
-    options, args = parser.parse_args()
+        parser = OptionParser("usage: %prog [options] [<suite> [testcase] [testcase] [...]]")
+        parser.add_option("-b", "--bpp", dest = "bpp",
+                type = "int",
+                help = "set pixel depth")
+        parser.add_option("-2", "--usepow2textures", dest = "usepow2textures",
+                type = "string",
+                action = 'callback', callback = setPow2,
+                help = "Use power of 2 textures (yes, no)")
+        parser.add_option("-s", "--useshaders", dest = "shaders",
+                type = "string",
+                action = 'callback', callback = setShaders,
+                help = "Use shaders (yes, no)")
+        parser.add_option("-p", "--usepixelbuffers", dest = "usepixelbuffers",
+                type = "string",
+                action = 'callback', callback = setPixBuf,
+                help = "Use pixel buffers (yes, no)")
+        parser.set_defaults(bpp = 24)
+        options, args = parser.parse_args()
 
-    availableSuites = {
-            'plugin': (pluginTestSuite,{}),
-            'player': (playerTestSuite, {'bpp':options.bpp}),
-            'image': (imageTestSuite, {}),
-            'vector': (vectorTestSuite, {}),
-            'words': (wordsTestSuite, {}),
-            'av': (AVTestSuite, {}),
-            'dynamics': (dynamicsTestSuite, {}),
-            'python': (pythonTestSuite, {}),
-            'anim': (animTestSuite, {}),
-            'event': (eventTestSuite, {}),
-            }
-    tests = []
-    if len(args): # suite
-        suiteName = args.pop(0)
-        if suiteName not in availableSuites:
-            parser.print_usage()
-            print "ERROR: unknown test suite, known suites:"
-            print ", ".join(availableSuites.keys())
-            sys.exit(1)
+        availableSuites = {
+                'plugin': (pluginTestSuite,{}),
+                'player': (playerTestSuite, {'bpp':options.bpp}),
+                'image': (imageTestSuite, {}),
+                'vector': (vectorTestSuite, {}),
+                'words': (wordsTestSuite, {}),
+                'av': (AVTestSuite, {}),
+                'dynamics': (dynamicsTestSuite, {}),
+                'python': (pythonTestSuite, {}),
+                'anim': (animTestSuite, {}),
+                'event': (eventTestSuite, {}),
+                }
+        tests = []
+        if len(args): # suite
+            suiteName = args.pop(0)
+            if suiteName not in availableSuites:
+                parser.print_usage()
+                print "ERROR: unknown test suite, known suites:"
+                print ", ".join(availableSuites.keys())
+                cleanExit(1)
+            else:
+                suitesToRun = [availableSuites[suiteName]]
+            tests = args
         else:
-            suitesToRun = [availableSuites[suiteName]]
-        tests = args
-    else:
-        suitesToRun = availableSuites.values()
+            suitesToRun = availableSuites.values()
 
-    suite = unittest.TestSuite()
-    for s, args in suitesToRun:
-        args.update({'tests':tests})
-        suite.addTest(s(**args))
+        suite = unittest.TestSuite()
+        for s, args in suitesToRun:
+            args.update({'tests':tests})
+            suite.addTest(s(**args))
 
-    Player = avg.Player.get()
-    Player.setMultiSampleSamples(1)
-    dumpConfig()
-    Log = avg.Logger.get()
+        Player = avg.Player.get()
+        Player.setMultiSampleSamples(1)
+        dumpConfig()
+        Log = avg.Logger.get()
 
-    runner = unittest.TextTestRunner()
-    rmBrokenDir()
-    rc = runner.run(suite)
-    
-    if rc.wasSuccessful():
-        sys.exit(0)
-    else:
-        sys.exit(1)
+        runner = unittest.TextTestRunner()
+        rmBrokenDir()
+        rc = runner.run(suite)
+        
+        if rc.wasSuccessful():
+            cleanExit(0)
+        else:
+            cleanExit(1)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print 'CTRL-C'
+        cleanExit(0)
 
