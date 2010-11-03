c8028dd1b52c517173b131ac93a189dca9874fa3
uzadow <uzadow@libavg.de>
2010-11-03 16:07:55 +0000

    Update of sample code.
    svn path=/trunk/libavg/; revision=5262

--- src/samples/mesh.py
+++ src/samples/mesh.py
@@ -8,11 +8,10 @@ player = avg.Player.get()
 player.loadString("""
 <avg width="160" height="120"/>
 """)
-mesh = player.createNode("mesh", {"texhref":"rgb24-64x64.png",
-        "vertexcoords":((0,0), (64,0), (0,64), (64, 64), (32, 32)),
-        "texcoords":((0,0), (1,0), (0,1), (1,1), (0.5,0.5)),
-        "triangles":((0,1,4), (1,3,4), (3,2,4), (2,0,4))
-    })
+mesh = avg.MeshNode(texhref="rgb24-64x64.png", 
+        vertexcoords=((0,0), (64,0), (0,64), (64, 64), (32, 32)),
+        texcoords=((0,0), (1,0), (0,1), (1,1), (0.5,0.5)),
+        triangles=((0,1,4), (1,3,4), (3,2,4), (2,0,4)))
 player.getRootNode().appendChild(mesh)
 player.setResolution(0,640,0,0)
 player.play()
--- src/samples/plugin.py
+++ src/samples/plugin.py
@@ -6,7 +6,8 @@ from libavg import avg
 player = avg.Player.get()
 
 player.loadString("""<avg width="160" height="120" id="container" />""")
-player.pluginPath = "my_pluginpath"
+# Change following line if the plugin is somewhere else.
+player.pluginPath = "../test/plugin/.libs"
 player.loadPlugin("libColorNode")
 
 node = player.createNode('<colornode fillcolor="0f3f7f" id="mynode" />')
