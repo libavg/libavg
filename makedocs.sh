dd78f0dfb5d13ee838eb009d430c41385006f300
uzadow <uzadow@libavg.de>
2010-11-26 15:13:48 +0000

    Reference generation script update.
    svn path=/trunk/libavg/; revision=5307

--- makedocs.sh
+++ makedocs.sh
@@ -1,2 +1,6 @@
-#/bin/bash
-epydoc --config=epydoc.cfg libavg
+#!/bin/bash
+
+cd sphinxdoc
+#sphinx-build -b linkcheck . _build/ 
+sphinx-build -b coverage . _build/
+sphinx-build -b html . _build/reference
--- sphinxdoc/makedocs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#sphinx-build -b linkcheck . _build/ 
-sphinx-build -b coverage . _build/
-sphinx-build -b html . _build/reference
