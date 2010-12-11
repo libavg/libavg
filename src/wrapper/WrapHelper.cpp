libavg/libavg

commit b5c4fd52ebe727994c5f7f6664763e908cbc0025

Fixes #175
svn path=/trunk/libavg/; revision=5329

src/wrapper/WrapHelper.cpp
@@ -168,8 +168,10 @@ struct point_from_python
     {
         POINT pt;
         PyObject * pEntry = PySequence_GetItem(obj_ptr, 0);
+        Py_DECREF(pEntry);
         pt.x = (ATTR)PyFloat_AsDouble(pEntry);
         pEntry = PySequence_GetItem(obj_ptr, 1);
+        Py_DECREF(pEntry);
         pt.y = (ATTR)PyFloat_AsDouble(pEntry);
         void* storage = (
                 (boost::python::converter::rvalue_from_python_storage<POINT>*)data)

commit 54b41c39edb19a5fb74bb3144dfc0a5139c9bb8a

Fixed triple mem leak & use after free caused by r5329.
svn path=/trunk/libavg/; revision=5331

src/wrapper/WrapHelper.cpp
@@ -168,11 +168,11 @@ struct point_from_python
     {
         POINT pt;
         PyObject * pEntry = PySequence_GetItem(obj_ptr, 0);
-        Py_DECREF(pEntry);
         pt.x = (ATTR)PyFloat_AsDouble(pEntry);
-        pEntry = PySequence_GetItem(obj_ptr, 1);
         Py_DECREF(pEntry);
+        pEntry = PySequence_GetItem(obj_ptr, 1);
         pt.y = (ATTR)PyFloat_AsDouble(pEntry);
+        Py_DECREF(pEntry);
         void* storage = (
                 (boost::python::converter::rvalue_from_python_storage<POINT>*)data)
                     ->storage.bytes;
@@ -207,10 +207,13 @@ struct triple_from_python
         avg::Triple<NUM> t;
         PyObject * pEntry = PySequence_GetItem(obj_ptr, 0);
         t.x = (NUM)PyFloat_AsDouble(pEntry);
+        Py_DECREF(pEntry);
         pEntry = PySequence_GetItem(obj_ptr, 1);
         t.y = (NUM)PyFloat_AsDouble(pEntry);
+        Py_DECREF(pEntry);
         pEntry = PySequence_GetItem(obj_ptr, 2);
         t.z = (NUM)PyFloat_AsDouble(pEntry);
+        Py_DECREF(pEntry);
         void* storage = (
                 (boost::python::converter::rvalue_from_python_storage<Triple<NUM> >*)
                         data)->storage.bytes;