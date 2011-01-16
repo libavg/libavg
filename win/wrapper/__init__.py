e3be1d058d120f946812df81a4adaddfcfa6ea6b
uzadow <uzadow@libavg.de>
2011-01-16 17:17:46 +0000

    win7: New build now builds avg.pyd, installs, runs all tests except plugin.
    svn path=/branches/win7/; revision=5394

---
+++ win/wrapper/__init__.py
@@ -0,0 +1,11 @@
+from avg import *
+import anim
+import draggable
+import camcalibrator
+import button
+import textarea
+from grabbable import Grabbable
+from AVGApp import AVGApp
+from AVGAppStarter import AVGAppStarter
+from AVGMTAppStarter import AVGMTAppStarter
+import AVGAppUtil
