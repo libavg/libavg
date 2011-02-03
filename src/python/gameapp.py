50f306c28fa7519fcf7e6ba6a24fca3ddd03ab63
oxullo <x@brainrapers.org>
2011-02-03 23:49:39 +0000

    Added a general purpose persistent data storage class
    svn path=/trunk/libavg/; revision=5476

--- src/python/gameapp.py
+++ src/python/gameapp.py
@@ -25,8 +25,11 @@ Transitional class to fulfill some of the missing features of AVGApp,
 currently needed for multitouch games.
 '''
 
+from __future__ import with_statement
 import os
 import sys
+import cPickle as pickle
+
 import libavg
 from libavg import avg
 
@@ -38,6 +41,138 @@ _app = None
 def app():
     global _app
     return _app
+
+
+class Datastore(object):
+    '''
+    General purpose persistent object (state in self.data)
+    
+    A possible use case:
+    
+    >>> from libavg import gameapp
+    
+    >>> def validate(data):
+    >>>    return type(data) == list and not isinstance(data[2], Bar)
+
+    >>> myInitialData = [1, 'foo', Bar(12)]
+    >>> myDs = gameapp.Datastore('hiscore', myInitialData, validate)
+    >>> myDs.data[2].foobarize(1)
+    >>> myDs.data[0] = 2
+    >>> myDs.commit()
+    
+    >>> againMyDs = gameapp.Datastore.get('hiscore')
+    '''
+    
+    instances = {}
+    
+    def __init__(self, tag, initialData=None, validator=lambda ds: True, autoCommit=True):
+        '''
+        tag: an application-wise unique string which is used to key the datastore and
+                set its dump filename.
+        initialData: a callable (class, method) or an instance that is used to initialize
+                the datastore when the dumpfile either doesn't exist, it's corrupted or
+                cannot be accessed. Defaults to an empty dict.
+        validator: a callable to which stored data are passed and that is able to
+                determine if the data format is expected. If the call returns True, the
+                proposed data are accepted and set. With False as return value the
+                Datastore uses initialData instead.
+        autoCommit: if True, the datastore sync itself when the program exits (uses
+                atexit). It's always possible to commit() manually.
+        '''
+        self.__tag = tag
+        self.__dumpFile = app().getUserdataPath(tag + '.pkl')
+
+        if tag in Datastore.instances:
+            raise RuntimeError('%s already initialized' % self)
+        
+        if hasattr(initialData, '__call__'):
+            initialData = initialData()
+        elif initialData is None:
+            initialData = dict()
+        
+        if os.path.exists(self.__dumpFile):
+            if not os.path.isfile:
+                raise RuntimeError('%s dump file '
+                        'is not a plain file' % self)
+            elif not os.access(self.__dumpFile, os.R_OK | os.W_OK):
+                raise RuntimeError('%s dump file'
+                        'cannot be accessed with r/w permissions' % self)
+
+        try:
+            f = open(self.__dumpFile)
+        except IOError:
+            g_Log.trace(g_Log.APP, 'Initializing %s' % self)
+            self.data = initialData
+            self.commit()
+        else:
+            try:
+                self.data = pickle.load(f)
+            except:
+                f.close()
+                g_Log.trace(g_Log.ERROR, 'Datastore %s is corrupted, '
+                        'reinitializing' % self)
+                self.data = initialData
+                self.commit()
+            else:
+                f.close()
+                if not validator(self.data):
+                    g_Log.trace(g_Log.ERROR, 'Sanity check failed for %s: '
+                            'reinitializing' % self)
+                    self.data = initialData
+                    self.commit()
+                else:
+                    g_Log.trace(g_Log.APP, '%s successfully '
+                            'loaded' % self)
+        
+        if autoCommit:
+            import atexit
+            atexit.register(self.commit)
+        
+        Datastore.instances[tag] = self
+    
+    @staticmethod
+    def get(tag):
+        '''
+        Retrieve a Datastore using its tag
+        '''
+        return Datastore.instances.get(tag, None)
+        
+    def commit(self):
+        '''
+        Dump Datastore data to disk
+        '''
+        import time
+        
+        tempFile = self.__dumpFile + '.tmp.' + str(int(time.time() * 1000))
+
+        try:
+            with open(tempFile, 'wb') as f:
+                pickle.dump(self.data, f)
+        except Exception, e:
+            g_Log.trace(g_Log.ERROR, 'Cannot save '
+                    '%s (%s)' % (self.__dumpFile, str(e)))
+            return False
+        else:
+            if os.path.exists(self.__dumpFile):
+                try:
+                    os.remove(self.__dumpFile)
+                except Exception, e:
+                    g_Log.trace(g_Log.ERROR, 'Cannot overwrite '
+                            'dump file %s (%s)' % (self, str(e)))
+                    return False
+            try:
+                os.rename(tempFile, self.__dumpFile)
+            except Exception, e:
+                g_Log.trace(g_Log.ERROR, 'Cannot save '
+                        '%s (%s)' % (self, str(e)))
+                os.remove(tempFile)
+                return False
+            else:
+                g_Log.trace(g_Log.APP, '%s saved' % self)
+                return True
+    
+    def __repr__(self):
+        return '<%s:%s %s>' % (self.__class__.__name__, self.__tag, self.__dumpFile)
 
 
 ownStarter = False
@@ -58,7 +193,7 @@ class GameApp(libavg.AVGApp):
         global _app
         _app = self
         super(GameApp, self).__init__(*args, **kwargs)
-
+        
         pkgpath = self._getPackagePath()
         if pkgpath is not None:
             avg.WordsNode.addFontDir(libavg.AVGAppUtil.getMediaDir(pkgpath, 'fonts'))
