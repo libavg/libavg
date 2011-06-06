6610e05d02da7a3c194776fd28c7f30ace16b3aa
uzadow <uzadow@libavg.de>
2011-06-06 13:48:29 +0000

    gestures branch: Indentation fix.
    svn path=/branches/libavg_gestures/; revision=6117

--- src/player/EventStream.h
+++ src/player/EventStream.h
@@ -34,55 +34,55 @@
 
 namespace avg {
 
-    class AVG_API EventStream
-    //internal class to keep track of blob/event states
-    {
-        public:
-            enum StreamState {
-                DOWN_PENDING, //fresh stream. not polled yet
-                DOWN_DELIVERED, //initial finger down delivered
-                MOTION_PENDING, //recent position change
-                MOTION_DELIVERED, //finger resting
-                VANISHED, // oops, no followup found -- wait a little while
-                UP_PENDING, //finger disappeared, but fingerup yet to be delivered
-                UP_DELIVERED // waiting to be cleared.
-            };
+class AVG_API EventStream
+//internal class to keep track of blob/event states
+{
+    public:
+        enum StreamState {
+            DOWN_PENDING, //fresh stream. not polled yet
+            DOWN_DELIVERED, //initial finger down delivered
+            MOTION_PENDING, //recent position change
+            MOTION_DELIVERED, //finger resting
+            VANISHED, // oops, no followup found -- wait a little while
+            UP_PENDING, //finger disappeared, but fingerup yet to be delivered
+            UP_DELIVERED // waiting to be cleared.
+        };
 
-            // State transitions:
-            // Current state       Destination state
-            // DOWN_PENDING     -> DOWN_DELIVERED (CURSORDOWN event), 
-            //                     UP_DELIVERED (spurious blob)
-            // DOWN_DELIVERED   -> VANISHED, MOTION_PENDING, MOTION_DELIVERED
-            // MOTION_PENDING   -> VANISHED, MOTION_DELIVERED (CURSORMOTION event)
-            // MOTION_DELIVERED -> VANISHED, MOTION_PENDING
-            // VANISHED         -> MOTION_PENDING, UP_PENDING
-            // UP_PENDING       -> UP_DELIVERED (CURSORUP event)
+        // State transitions:
+        // Current state       Destination state
+        // DOWN_PENDING     -> DOWN_DELIVERED (CURSORDOWN event), 
+        //                     UP_DELIVERED (spurious blob)
+        // DOWN_DELIVERED   -> VANISHED, MOTION_PENDING, MOTION_DELIVERED
+        // MOTION_PENDING   -> VANISHED, MOTION_DELIVERED (CURSORMOTION event)
+        // MOTION_DELIVERED -> VANISHED, MOTION_PENDING
+        // VANISHED         -> MOTION_PENDING, UP_PENDING
+        // UP_PENDING       -> UP_DELIVERED (CURSORUP event)
 
-            EventStream(BlobPtr pFirstBlob, long long time);
-            virtual ~EventStream();
-            void blobChanged(BlobPtr pNewBlob, long long time, bool bEventOnMove);
-            void blobGone();
-            EventPtr pollevent(DeDistortPtr pDeDistort, const DRect& displayROI, 
-                    CursorEvent::Source source, bool bEventOnMove);
-            bool isGone();
-            void setStale();
-            bool isStale();
-            void dump();
-            static std::string stateToString(StreamState state);
+        EventStream(BlobPtr pFirstBlob, long long time);
+        virtual ~EventStream();
+        void blobChanged(BlobPtr pNewBlob, long long time, bool bEventOnMove);
+        void blobGone();
+        EventPtr pollevent(DeDistortPtr pDeDistort, const DRect& displayROI, 
+                CursorEvent::Source source, bool bEventOnMove);
+        bool isGone();
+        void setStale();
+        bool isStale();
+        void dump();
+        static std::string stateToString(StreamState state);
 
-        private:
-            DPoint getSpeed(const DPoint& oldPos, const DPoint& newPos);
+    private:
+        DPoint getSpeed(const DPoint& oldPos, const DPoint& newPos);
 
-            bool m_Stale;
-            int m_ID;
-            StreamState m_State;
-            DPoint m_Pos;
-            DPoint m_OldPos;
-            BlobPtr m_pBlob;
-            long long m_Time;
-            long long m_OldTime;
-            static int s_LastLabel;
-    };
+        bool m_Stale;
+        int m_ID;
+        StreamState m_State;
+        DPoint m_Pos;
+        DPoint m_OldPos;
+        BlobPtr m_pBlob;
+        long long m_Time;
+        long long m_OldTime;
+        static int s_LastLabel;
+};
 
 }
 
