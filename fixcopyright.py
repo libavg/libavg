1f42fcf11d197b86906f7aafa5ff8a1d3854a09b
uzadow <uzadow@libavg.de>
2011-08-29 21:11:48 +0000

    Fixed obvious copyright statement bugs; removed very old broken files.
    svn path=/trunk/libavg/; revision=6569

--- fixcopyright.py
+++ fixcopyright.py
@@ -4,7 +4,6 @@ import os
 import re
 
 def handleFile(path):
-    print path,
     lines = file(path).readlines()
     lineNumber = 0
     found = False
@@ -12,24 +11,17 @@ def handleFile(path):
         match = re.match(r'((//)|(#))\s*Copyright \(C\) 2003-(.*) Ulrich von Zadow\s*', line)
 #        m = re.match(r'#include\s*["<]([\-_a-zA-Z0-9\.\\/]+)[">]\s*', l)
         if match:
-            print path[-2:]
             if path[-2:] == 'py':
                 lines[i] = "# Copyright (C) 2003-2011 Ulrich von Zadow\n"
             else:
                 lines[i] = "//  Copyright (C) 2003-2011 Ulrich von Zadow\n"
             found = True
-    if found:
-        content = "".join(lines)
-        file(path, "w").write(content)
-        print
-    else:
-        print " -> no copyright"
-
+    if not(found):
+        print path
 
 
 for ext in ("h", "c", "cpp", "py"):
     cmd = 'find . -name "*.'+ext+'"'
-    print cmd
     files = os.popen(cmd).readlines()
     for f in files:
         handleFile(f.strip())
