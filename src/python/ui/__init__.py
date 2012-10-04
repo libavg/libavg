214113f4c7b8a31e4c21fad5ea57b9a5092683c4
uzadow <uzadow@libavg.de>
2012-10-04 12:45:04 +0000

    uilib2 branch: Moved SwitchNode and AccordionNode to new ui.base module.
    svn path=/branches/libavg_uilib2/; revision=8061

--- src/python/ui/__init__.py
+++ src/python/ui/__init__.py
@@ -1,5 +1,6 @@
-from button import SwitchNode, Button, BmpButton, ToggleButton, BmpToggleButton
+from base import SwitchNode, AccordionNode, Orientation
+from button import Button, BmpButton, ToggleButton, BmpToggleButton
 from keyboard import Keyboard
 from gesture import Recognizer, TapRecognizer, DragRecognizer, DoubletapRecognizer, TransformRecognizer, calcKMeans, Mat3x3, Transform, HoldRecognizer
 from scrollarea import ScrollPane, ScrollArea
-from slider import Orientation, AccordionNode, Slider, ScrollBar, ScrollBarTrack, ScrollBarThumb
+from slider import Slider, ScrollBar, ScrollBarTrack, ScrollBarThumb
