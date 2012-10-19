66b2e34ae97632b49a6f92d2f8fdce583c686e79
uzadow <uzadow@libavg.de>
2012-10-19 19:52:10 +0000

    gles branch: Merge from trunk.
    svn path=/branches/libavg_gles/; revision=8133

--- src/python/ui/__init__.py
+++ src/python/ui/__init__.py
@@ -2,4 +2,4 @@ from button import SwitchNode, Button, BmpButton, ToggleButton, BmpToggleButton
 from keyboard import Keyboard
 from gesture import Recognizer, TapRecognizer, DragRecognizer, DoubletapRecognizer, TransformRecognizer, calcKMeans, Mat3x3, Transform, HoldRecognizer
 from scrollarea import ScrollPane, ScrollArea
-from slider import Orientation, AccordionNode, Slider, BmpSlider, ScrollBar, ScrollBarTrack, ScrollBarThumb, BmpScrollBar
+from slider import Orientation, AccordionNode, Slider, BmpSlider, SliderThumb, ScrollBar, ScrollBarTrack, ScrollBarThumb, BmpScrollBar
