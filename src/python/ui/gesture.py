695c820eff492209eb6fc4a493878d054da43893
uzadow <uzadow@libavg.de>
2012-12-10 14:10:39 +0000

    After a gesture fail, stop listening to contacts.
    svn path=/trunk/libavg/; revision=8488

--- src/python/ui/gesture.py
+++ src/python/ui/gesture.py
@@ -104,6 +104,10 @@ class Recognizer(avg.Publisher):
         assert(self.__stateMachine.state != "RUNNING")
         if self.__stateMachine.state != "IDLE":
             self.__stateMachine.changeState("IDLE")
+        for contact in self._contacts:
+            contact.unsubscribe(avg.Contact.CURSOR_MOTION, self.__onMotion)
+            contact.unsubscribe(avg.Contact.CURSOR_UP, self.__onUp)
+        self._contacts = set()
         self.notifySubscribers(Recognizer.FAILED, [])
 
     def _setDetected(self, event):
