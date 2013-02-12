904c4a2c1fd39398a950105f8f1d3d60041f782d
klemmster <richy@coding-reality.de>
2013-02-12 17:07:14 +0000

    logging branch: Added logsample
    svn path=/branches/experiments/libavg_logging_refactor/; revision=8796

---
+++ src/samples/logsample.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+from libavg import avg, AVGApp
+
+# Setup Python Logger
+hdlr = logging.StreamHandler()
+# category is added as an extra formatting key by libavg
+formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(category)s] : %(message)s')
+hdlr.setFormatter(formatter)
+pyLogger = logging.getLogger(__name__)
+pyLogger.addHandler(hdlr)
+pyLogger.propagate = False
+
+
+class LoggingApp(AVGApp):
+    def init(self):
+        # Add the python logger to libavgs logger as a message sink
+        avg.logger.addSink(pyLogger)
+
+        avg.logger.log("Custom Info level message",avg.logger.APP, avg.logger.INFO)
+
+        avg.logger.debug("Debug level message, with APP Category")
+        avg.logger.info("Info level message, with APP Category")
+        avg.logger.warning("Warn level message, with APP Category")
+
+        #Remove the logSink, no message should be logged now, if run with
+        #AVG_LOG_OMIT_STDERR=1
+        avg.logger.removeSink(logging.getLogger("MY_APP"))
+
+        avg.logger.error("std::err - Error")
+        avg.logger.critical("std::err - Critical")
+        avg.logger.log("std::err - Log")
+
+LoggingApp.start(resolution=(40, 40))
