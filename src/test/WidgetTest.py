abeddee0dc3b73e74d83a706f9acfb2062e33e58
klemmster <richy@coding-reality.de>
2013-04-04 10:11:53 +0000

    Added test for custom scroll area skin
    svn path=/trunk/libavg/; revision=9015

--- src/test/WidgetTest.py
+++ src/test/WidgetTest.py
@@ -847,6 +847,18 @@ class WidgetTestCase(AVGTestCase):
                  lambda: self.compareImage("testScrollArea2"),
                 ))
 
+    def testScrollAreaCustomSkin(self):
+        root = self.loadEmptyScene()
+        image = avg.ImageNode(href="rgb24-64x64.png", size=(200, 400))
+        pwdPath = os.path.dirname(os.path.realpath(__file__))
+        mediaPath = os.path.join(pwdPath, "media")
+        skin = widget.Skin(os.path.join(pwdPath, "media/CustomSkin.xml"),
+                mediaPath)
+        self.node = widget.ScrollArea(contentNode=image, size=(80, 80), skinObj=skin,
+                parent=root)
+        self.start(False,
+                (lambda: self.compareImage("testScrollArea3"),))
+
     def testCustomMediaDir(self):
         root = self.loadEmptyScene()
 
@@ -877,6 +889,7 @@ def widgetTestSuite(tests):
         "testProgressBar",
         "testMediaControl",
         "testScrollArea",
+        "testScrollAreaCustomSkin",
         "testCustomMediaDir",
         )
 
