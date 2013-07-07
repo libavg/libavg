b6eb311715df0cc375f186399f37d8d320c3c244
oxullo <x@brainrapers.org>
2013-07-07 21:28:55 +0000

    libavg_app: samples updated
    svn path=/branches/libavg_app/; revision=9201

--- src/samples/twovideos.py
+++ src/samples/twovideos.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from libavg import *
 import sys
+import libavg
+from libavg import avg, app, player
 
-FADE_DURATION = 2000
 
-class HDVideoApp(AVGApp):
-    def init(self):
+class HDVideo(app.MainDiv):
+    def onArgvParserCreated(self, parser):
+        parser.set_usage('%prog <video1> <video2>')
+        parser.add_option('--duration', '-d', dest='duration',
+                default=2000, type='int', help='Crossfade duration')
+
+    def onArgvParsed(self, options, args, parser):
+        if len(args) != 2:
+            parser.print_help()
+            sys.exit(1)
+
+        self.__videos = args
+        self.__duration = options.duration
+
+    def onInit(self):
         self.videoNodes = []
-        for fileName in (sys.argv[1], sys.argv[2]):
-            videoNode = VideoNode(
-                    size=(1680, 1050), 
+        for fileName in (self.__videos[0], self.__videos[1]):
+            videoNode = avg.VideoNode(
+                    size=(1680, 1050),
                     href=fileName,
                     opacity=0,
                     loop=True,
-                    parent=self._parentNode)
+                    parent=self)
             videoNode.play()
             self.videoNodes.append(videoNode)
 
@@ -23,21 +36,20 @@ class HDVideoApp(AVGApp):
         self.runningVideo = 0
         self.isFading = False
 
-    def onKeyDown(self, event):
-        if event.keystring == '1':
-            if not(self.isFading):
-                if self.runningVideo == 0:
-                    fadeIn(self.videoNodes[1], FADE_DURATION)
-                else:
-                    fadeOut(self.videoNodes[1], FADE_DURATION)
-                player.setTimeout(FADE_DURATION, self.fadeEnd)
-                self.runningVideo = (self.runningVideo+1)%2
-                self.isFading = True
-            return True
-        else:
-            return False
+        app.keyboardmanager.bindKeyDown('1', self.onButtonPressed,
+                'Crossfade between videos')
+
+    def onButtonPressed(self):
+        if not(self.isFading):
+            if self.runningVideo == 0:
+                avg.fadeIn(self.videoNodes[1], self.__duration)
+            else:
+                avg.fadeOut(self.videoNodes[1], self.__duration)
+            player.setTimeout(self.__duration, self.fadeEnd)
+            self.runningVideo = (self.runningVideo+1)%2
+            self.isFading = True
 
     def fadeEnd(self):
         self.isFading = False
         
-HDVideoApp.start(resolution=(1680, 1050), debugWindowSize=(720, 450))
+app.App().run(HDVideo(), app_resolution='1680x1050', app_window_size='720x450')
