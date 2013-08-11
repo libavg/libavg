31897536e02280e912c1561802d07307711d48d9
uzadow <uzadow@libavg.de>
2013-08-11 20:43:59 +0000

    Made BitmapManager usable from C++.
    svn path=/trunk/libavg/; revision=9275

---
+++ src/player/IBitmapLoadedListener.h
@@ -0,0 +1,44 @@
+//
+//  libavg - Media Playback Engine. 
+//  Copyright (C) 2003-2011 Ulrich von Zadow
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  Current versions can be found at www.libavg.de
+//
+
+#ifndef _IBitmapLoadedListener_H_ 
+#define _IBitmapLoadedListener_H_
+
+#include "../api.h"
+
+#include <boost/shared_ptr.hpp>
+
+namespace avg {
+
+class Bitmap;
+typedef boost::shared_ptr<Bitmap> BitmapPtr;
+class Exception;
+
+class AVG_API IBitmapLoadedListener {
+public:
+    virtual ~IBitmapLoadedListener() {};
+    virtual void onBitmapLoaded(BitmapPtr pBmp) = 0;
+    virtual void onBitmapLoadError(const Exception* e) = 0;
+};
+
+}
+
+#endif
