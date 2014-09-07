b57f2b98b8b9377c46c834a3e7fcac467c9811ee
Ulrich von Zadow <uzadow@libavg.de>
2014-09-07 18:41:26 +0200

    Replaced assertException with assertRaises in WordsTest.

--- src/test/WordsTest.py
+++ src/test/WordsTest.py
@@ -133,12 +133,12 @@ class WordsTestCase(AVGTestCase):
         self.assert_(posAlmostEqual(node.getGlyphPos(3), (22,0)))
         size = node.getGlyphSize(3)
         self.assert_(posAlmostEqual(size, (8, 18)))
-        self.assertException(lambda: node.getGlyphPos(4))
+        self.assertRaises(RuntimeError, lambda: node.getGlyphPos(4))
         node.text=u"föa"
         self.assert_(posAlmostEqual(node.getGlyphPos(1), (4,0)))
 #        print [ node.getGlyphPos(i) for i in range(3)]
         self.assert_(posAlmostEqual(node.getGlyphPos(2), (12,0)))
-        self.assertException(lambda: node.getGlyphPos(3))
+        self.assertRaises(RuntimeError, lambda: node.getGlyphPos(3))
 
     def testParaWords(self):
         root = self.loadEmptyScene()
@@ -426,8 +426,7 @@ class WordsTestCase(AVGTestCase):
         def bombIt():
             def cantRun():
                 self.xmldnode.rawtextmode = False
-                self.assert_(0)
-            self.assertException(cantRun)
+            self.assertRaises(RuntimeError, cantRun)
         
         def assignNewTexts():
             text = u'&ùùààxx>'
@@ -573,9 +572,9 @@ class WordsTestCase(AVGTestCase):
 
         self.loadEmptyScene()
         self.start(True, 
-                (self.assertException(assignInvalidColor1),
-                 self.assertException(assignInvalidColor2),
-                 self.assertException(assignInvalidColor3),
+                (self.assertRaises(RuntimeError, assignInvalidColor1),
+                 self.assertRaises(RuntimeError, assignInvalidColor2),
+                 self.assertRaises(RuntimeError, assignInvalidColor3),
                 ))
 
     def testFontDir(self):
@@ -648,9 +647,7 @@ class WordsTestCase(AVGTestCase):
         root = self.loadEmptyScene()
         text = "42 " * 42 * 20 
         avg.WordsNode(parent=root, text=text)
-        self.assertException(
-                lambda: self.start((None, None))
-        )
+        self.assertRaises(RuntimeError, lambda: self.start(True, (None, None)))
 
     def testWordsGamma(self):
         
