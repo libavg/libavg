203b61d3c158a86f662303d5b3eb70d40c415d61
Ulrich von Zadow <uzadow@libavg.de>
2014-09-07 17:07:09 +0200

    Added texcoord interpolation for PolyLine and Polygon.

--- src/test/VectorTest.py
+++ src/test/VectorTest.py
@@ -386,7 +386,7 @@ class VectorTestCase(AVGTestCase):
         
         def repeatTexCoords():
             polyline.pos = [(10,10), (30,10), (30,50), (50,50), (50,70), (70,70)]
-            polyline.texcoords = [1, 2, 3]
+            polyline.texcoords = [0,1]
         
         canvas = self.makeEmptyCanvas()
         polyline = texturePolyLine()
