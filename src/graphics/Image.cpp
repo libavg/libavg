1c0fd7aad2bddd4176386a36f4e0aa142e08409f
Ulrich von Zadow <uzadow@libavg.de>
2015-01-03 16:05:44 +0100

    Initial (unused) ref-counting image class.

---
+++ src/graphics/Image.cpp
@@ -0,0 +1,143 @@
+//
+//  libavg - Media Playback Engine. 
+//  Copyright (C) 2003-2014 Ulrich von Zadow
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  Current versions can be found at www.libavg.de
+//
+
+#include "Image.h"
+
+#include "../base/Logger.h"
+#include "../base/Exception.h"
+#include "../base/ObjectCounter.h"
+
+#include "BitmapLoader.h"
+#include "Bitmap.h"
+#include "GLContextManager.h"
+#include "MCTexture.h"
+
+using namespace std;
+
+namespace avg {
+
+Image::Image(const std::string& sFilename)
+    : m_bUseMipmaps(false),
+      m_Compression(TEXTURECOMPRESSION_NONE),
+      m_BmpRefCount(0),
+      m_TexRefCount(0)
+{
+    m_sFilename = sFilename;
+    m_pBmp = loadBitmap(sFilename);
+    incBmpRef();
+}
+
+Image::Image(const BitmapPtr& pBmp)
+    : m_bUseMipmaps(false),
+      m_Compression(TEXTURECOMPRESSION_NONE),
+      m_BmpRefCount(0),
+      m_TexRefCount(0)
+{
+    m_sFilename = "";
+    m_pBmp = BitmapPtr(new Bitmap(pBmp->getSize(), pBmp->getPixelFormat(), ""));
+    m_pBmp->copyPixels(*pBmp);
+    incBmpRef();
+}
+
+Image::~Image()
+{
+    AVG_ASSERT(m_BmpRefCount == 0);
+    AVG_ASSERT(m_TexRefCount == 0);
+}
+
+std::string Image::getFilename() const
+{
+    return m_sFilename;
+}
+
+void Image::incBmpRef()
+{
+    m_BmpRefCount++;
+}
+
+void Image::decBmpRef()
+{
+    AVG_ASSERT(m_BmpRefCount >= 1);
+    m_BmpRefCount--;
+
+    // TODO: If both refcounts == 0, tell ImageRegistry to remove image.
+}
+
+void Image::incTexRef(bool bUseMipmaps, Image::TextureCompression tc)
+{
+    // TODO: Handle mipmaps, wrap modes, compression
+    m_TexRefCount++; 
+    if (m_TexRefCount == 1) {
+        m_pTex = GLContextManager::get()->createTexture(m_pBmp->getSize(), 
+                m_pBmp->getPixelFormat(), m_bUseMipmaps, 
+                GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
+        GLContextManager::get()->scheduleTexUpload(m_pTex, m_pBmp);
+    }
+}
+
+void Image::decTexRef()
+{
+    AVG_ASSERT(m_BmpRefCount >= 1);
+    m_TexRefCount--; 
+    if (m_TexRefCount == 0) {
+        m_pTex = MCTexturePtr();
+    }
+}
+
+BitmapPtr Image::getBmp()
+{
+    AVG_ASSERT(m_BmpRefCount >= 1);
+    return m_pBmp;
+}
+
+MCTexturePtr Image::getTex()
+{
+    AVG_ASSERT(m_TexRefCount >= 1);
+    return m_pTex;
+}
+
+Image::TextureCompression Image::string2compression(const string& s)
+{
+    if (s == "none") {
+        return Image::TEXTURECOMPRESSION_NONE;
+    } else if (s == "B5G6R5") {
+        return Image::TEXTURECOMPRESSION_B5G6R5;
+    } else {
+        throw(Exception(AVG_ERR_UNSUPPORTED, 
+                "GPUImage compression "+s+" not supported."));
+    }
+}
+
+string Image::compression2String(TextureCompression compression)
+{
+    switch(compression) {
+        case Image::TEXTURECOMPRESSION_NONE:
+            return "none";
+        case Image::TEXTURECOMPRESSION_B5G6R5:
+            return "B5G6R5";
+        default:
+            AVG_ASSERT(false);
+            return 0;
+    }
+}
+
+
+}
---
+++ src/graphics/Image.h
@@ -0,0 +1,82 @@
+//
+//  libavg - Media Playback Engine. 
+//  Copyright (C) 2003-2014 Ulrich von Zadow
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  Current versions can be found at www.libavg.de
+//
+
+#ifndef _Image_H_
+#define _Image_H_
+
+#include "../api.h"
+
+#include "../base/GLMHelper.h"
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+
+namespace avg {
+
+class Bitmap;
+typedef boost::shared_ptr<Bitmap> BitmapPtr;
+class MCTexture;
+typedef boost::shared_ptr<MCTexture> MCTexturePtr;
+
+class AVG_API Image
+{
+    public:
+        enum TextureCompression {
+            TEXTURECOMPRESSION_NONE,
+            TEXTURECOMPRESSION_B5G6R5
+        };
+
+        Image(const std::string& sFilename);
+        Image(const BitmapPtr& pBmp);
+        virtual ~Image();
+
+        std::string getFilename() const;
+
+        void incBmpRef();
+        void decBmpRef();
+        void incTexRef(bool bUseMipmaps=false,
+                TextureCompression tc=TEXTURECOMPRESSION_NONE);
+        void decTexRef();
+
+        BitmapPtr getBmp();
+        MCTexturePtr getTex();
+
+        static TextureCompression string2compression(const std::string& s);
+        static std::string compression2String(TextureCompression compression);
+
+    private:
+        std::string m_sFilename;
+        BitmapPtr m_pBmp;
+        MCTexturePtr m_pTex;
+
+        bool m_bUseMipmaps;
+        TextureCompression m_Compression;
+        
+        int m_BmpRefCount;
+        int m_TexRefCount;
+};
+
+typedef boost::shared_ptr<Image> ImagePtr;
+
+}
+
+#endif
+
