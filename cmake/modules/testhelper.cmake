e45a29b9b88d29b951b92fb39c6613771bd9897b
hth <hth@archimedes-exhibitions.de>
2016-05-04 16:30:27 +0200

    Remove copy_python_test target dependency from cmake testhelper macros

--- cmake/modules/testhelper.cmake
+++ cmake/modules/testhelper.cmake
@@ -1,6 +1,5 @@
 macro(addTestToLibavgPythonPackage testTarget)
     add_custom_target(copy_${testTarget} ALL)
-    add_dependencies(copy_${testTarget} ${testTarget} copy_python_tests)
     add_custom_command(TARGET copy_${testTarget}
         COMMAND ${CMAKE_COMMAND} -E copy
         "${CMAKE_CURRENT_BINARY_DIR}/${testTarget}"
@@ -18,7 +17,6 @@ endmacro()
 
 macro(addPluginToLibavgPythonPackage plugin)
     add_custom_target(copy_${plugin} ALL)
-    add_dependencies(copy_${plugin} ${plugin} copy_python_tests)
     add_custom_command(TARGET copy_${plugin}
         COMMAND ${CMAKE_COMMAND} -E copy
         "${CMAKE_CURRENT_BINARY_DIR}/${plugin}${CMAKE_SHARED_MODULE_SUFFIX}"
