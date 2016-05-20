05f0e04598c44041311de93bbf8ce1f96c9da217
hth <hth@archimedes-exhibitions.de>
2016-05-20 15:38:54 +0200

    Fix cmake copy_directory error during parallel build

--- cmake/modules/testhelper.cmake
+++ cmake/modules/testhelper.cmake
@@ -1,4 +1,4 @@
-macro(addTestToLibavgPythonPackage testTarget)
+macro(addTestTargetToLibavgPythonPackage testTarget)
     add_custom_target(copy_${testTarget} ALL)
     add_dependencies(copy_${testTarget} ${testTarget})
     add_custom_command(TARGET copy_${testTarget}
@@ -6,15 +6,17 @@ macro(addTestToLibavgPythonPackage testTarget)
         "${CMAKE_CURRENT_BINARY_DIR}/${testTarget}"
         "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest"
         )
-
-    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/baseline")
-        add_custom_command(TARGET copy_${testTarget}
-            COMMAND ${CMAKE_COMMAND} -E copy_directory
-            "${CMAKE_CURRENT_SOURCE_DIR}/baseline"
-            "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest/baseline"
-            )
-    endif()
 endmacro()
+
+
+macro(addTestDataToLibavgPythonPackage testTarget dataDir)
+    add_custom_command(TARGET copy_${testTarget}
+        COMMAND ${CMAKE_COMMAND} -E copy_directory
+        "${CMAKE_CURRENT_SOURCE_DIR}/${dataDir}"
+        "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest/baseline"
+        )
+endmacro()
+
 
 macro(addPluginToLibavgPythonPackage plugin)
     add_custom_target(copy_${plugin} ALL)
