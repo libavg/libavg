574df9f0fe11475e7fc4775ddc2977092f343ace
Ulrich von Zadow <uzadow@libavg.de>
2016-05-27 22:17:54 +0200

    Only copy test files to staging area if they've changed.

--- cmake/modules/testhelper.cmake
+++ cmake/modules/testhelper.cmake
@@ -1,37 +1,36 @@
 function(addTestTargetToLibavgPythonPackage testTarget)
     add_custom_target(copy_${testTarget} ALL)
     add_dependencies(copy_${testTarget} ${testTarget})
-    add_custom_command(TARGET copy_${testTarget}
-        COMMAND ${CMAKE_COMMAND} -E copy
-        "${CMAKE_CURRENT_BINARY_DIR}/${testTarget}"
-        "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest"
-        )
+    copyDirIfDifferent("copy_${testTarget}"
+            "${CMAKE_CURRENT_BINARY_DIR}/${testTarget}"
+            "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest")
 endfunction()
 
 
 function(addTestDataToLibavgPythonPackage testTarget dataDir)
-    add_custom_command(TARGET copy_${testTarget}
-        COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${CMAKE_CURRENT_SOURCE_DIR}/${dataDir}"
-        "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest/baseline"
-        )
+    copyDirIfDifferent("copy_${testTarget}"
+            "${CMAKE_CURRENT_SOURCE_DIR}/${dataDir}"
+            "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest/baseline")
 endfunction()
 
 function(copyToStaging dir)
     set(python_test_package_dir "${CMAKE_BINARY_DIR}/python/libavg/test")
-    add_custom_command(TARGET copy_python_tests
+    copyDirIfDifferent(copy_python_tests
+            "${CMAKE_CURRENT_SOURCE_DIR}/${dir}"
+            "${python_test_package_dir}/${dir}")
+endfunction()
+
+function(copyDirIfDifferent target src_dir dest_dir)
+    add_custom_command(TARGET ${target}
         COMMAND ${CMAKE_COMMAND}
-                ARGS -E make_directory "${python_test_package_dir}/${dir}"
+                ARGS -E make_directory "${dest_dir}"
         )
-    file(MAKE_DIRECTORY "${python_test_package_dir}/${dir}")
-    file(GLOB test_files
-            RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}/${dir}"
-            "${CMAKE_CURRENT_SOURCE_DIR}/${dir}/*")
-    foreach(cur_file ${test_files})
-        add_custom_command(TARGET copy_python_tests
+    file(GLOB filelist
+            RELATIVE "${src_dir}" "${src_dir}/*")
+    foreach(cur_file ${filelist})
+        add_custom_command(TARGET ${target}
             COMMAND ${CMAKE_COMMAND}
-            ARGS -E copy_if_different "${CMAKE_CURRENT_SOURCE_DIR}/${dir}/${cur_file}"
-                    "${python_test_package_dir}/${dir}/${cur_file}"
+            ARGS -E copy_if_different "${src_dir}/${cur_file}" "${dest_dir}/${cur_file}"
             )
     endforeach()
 endfunction()
