7cc877ebb41162b120999912c464392373b4ed4a
Ulrich von Zadow <uzadow@libavg.de>
2016-05-27 21:56:51 +0200

    Added copyToStaging CMake function.

--- cmake/modules/testhelper.cmake
+++ cmake/modules/testhelper.cmake
@@ -16,3 +16,22 @@ function(addTestDataToLibavgPythonPackage testTarget dataDir)
         "${CMAKE_BINARY_DIR}/python/libavg/test/cpptest/baseline"
         )
 endfunction()
+
+function(copyToStaging dir)
+    set(python_test_package_dir "${CMAKE_BINARY_DIR}/python/libavg/test")
+    add_custom_command(TARGET copy_python_tests
+        COMMAND ${CMAKE_COMMAND}
+                ARGS -E make_directory "${python_test_package_dir}/${dir}"
+        )
+    file(MAKE_DIRECTORY "${python_test_package_dir}/${dir}")
+    file(GLOB test_files
+            RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}/${dir}"
+            "${CMAKE_CURRENT_SOURCE_DIR}/${dir}/*")
+    foreach(cur_file ${test_files})
+        add_custom_command(TARGET copy_python_tests
+            COMMAND ${CMAKE_COMMAND}
+            ARGS -E copy_if_different "${CMAKE_CURRENT_SOURCE_DIR}/${dir}/${cur_file}"
+                    "${python_test_package_dir}/${dir}/${cur_file}"
+            )
+    endforeach()
+endfunction()
