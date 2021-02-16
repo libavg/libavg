4544fe95896e08520ae425a3c1aa1a0f6245f2fa
Scotty <scotty@c-base.org>
2021-02-16 14:44:37 +0100

    Rename and update fixcopyright.py helper script (exclude unused/unmaintained folders)

--- update_copyright.py
+++ update_copyright.py
@@ -24,44 +24,57 @@ import os
 import re
 import time
 
+DIRS_AND_EXTENSIONS = (
+    ('src', ('.h', '.cpp', '.py', '.frag', '.vert')),
+    ('python', ('.py',)),
+    ('python/scripts/*', ('',)),  # w/o extensions
+    ('samples', ('.h', '.cpp', '.py')),
+)
 TEMPLATE = 'Copyright (C) %s-%s Ulrich von Zadow\n'
+
+_REGEXP = re.compile(r'(?P<comment>((// )|(#)))\s*Copyright \(C\) (?P<from>\d{4})(.*) Ulrich von Zadow\s*')
 
 num_changed = 0
 num_skipped = 0
 
 
-def handleFile(path):
+def update_file(path):
     global num_changed, num_skipped
 
     lines = file(path).readlines()
     found = False
     for i, line in enumerate(lines):
-        match = re.match(r'((//)|(#))\s*Copyright \(C\) (?P<from>\d{4})(.*) Ulrich von Zadow\s*', line)
+        match = _REGEXP.match(line)
         if match:
+            comment = match.group('comment')
             year_from = match.group('from')
             year_to = str(time.localtime().tm_year)
-            if path[-2:] == 'py':
-                lines[i] = '# ' + TEMPLATE % (year_from, year_to)
-            else:
-                lines[i] = '//  ' + TEMPLATE % (year_from, year_to)
+            lines[i] = comment + ' ' + TEMPLATE % (year_from, year_to)
             found = True
             break
     if found:
-        num_changed +=1
-        outFile = open(path, "w")
-        for line in lines:
-            outFile.write(line)
+        num_changed += 1
+        with open(path, "w") as out_file:
+            for line in lines:
+                out_file.write(line)
     else:
-        num_skipped +=1
+        num_skipped += 1
         print 'skipping', path
 
 
-# TODO: include src/graphics/shaders/*, python/scripts/*
-for ext in ("h", "c", "cpp", "py"):
-    cmd = 'find . -name "*.'+ext+'" -not -wholename "'+__file__+'"'
-    files = os.popen(cmd).readlines()
-    for f in files:
-        handleFile(f.strip())
+# top-level (helper) files
+cmd = 'ls *.py'
+files = os.popen(cmd).readlines()
+for f in files:
+    update_file(f.strip())
+
+# source (sub-)directories
+for (dir_, exts) in DIRS_AND_EXTENSIONS:
+    for ext in exts:
+        cmd = 'find ' + dir_ + ' -name "*' + ext + '"'
+        files = os.popen(cmd).readlines()
+        for f in files:
+            update_file(f.strip())
 
 print 'changed:', num_changed
 print 'skipped:', num_skipped
